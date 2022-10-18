39ea841886eb9b6364da0e94ede0c26f4b8dbc37
Rob Walworth <robert.walworth@swirldslabs.com>
2022-10-18 16:05:55 -0500

    Add TransactionReceipt and ExchangeRateSet classes
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

---
+++ sdk/main/include/ExchangeRateSet.h
@@ -0,0 +1,76 @@
+/*-
+ *
+ * Hedera C++ SDK
+ *
+ * Copyright (C) 2020 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#ifndef EXCHANGE_RATE_SET_H_
+#define EXCHANGE_RATE_SET_H_
+
+#include "ExchangeRate.h"
+
+#include <optional>
+
+namespace proto
+{
+class ExchangeRateSet;
+}
+
+namespace Hedera
+{
+/**
+ * Two sets of exchange rates.
+ */
+class ExchangeRateSet
+{
+public:
+  /**
+   * Create an ExchangeRateSet from a protobuf ExchangeRateSet.
+   *
+   * @param exchangeRateSet The protobuf ExchangeRateSet.
+   * @return An ExchangeRateSet with the protobuf ExchangeRateSet data.
+   */
+  static ExchangeRateSet fromProtobuf(const proto::ExchangeRateSet& proto);
+
+  /**
+   * Extract the current exchange rate.
+   *
+   * @return The current exchange rate.
+   */
+  inline std::optional<ExchangeRate> getCurrentExchangeRate() const { return mCurrentRate; }
+
+  /**
+   * Extract the next exchange rate.
+   *
+   * @return The next exchange rate.
+   */
+  inline std::optional<ExchangeRate> getNextExchangeRate() const { return mNextRate; }
+
+private:
+  /**
+   * The current exchange rate.
+   */
+  std::optional<ExchangeRate> mCurrentRate;
+
+  /**
+   * The next exchange rate which will take effect when current rate expires.
+   */
+  std::optional<ExchangeRate> mNextRate;
+};
+
+} // namespace Hedera
+
+#endif // EXCHANGE_RATE_SET_H_
