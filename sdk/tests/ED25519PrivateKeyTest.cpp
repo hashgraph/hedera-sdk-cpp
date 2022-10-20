8d33cd40b6afa1bcca911d29e64b8ec1db406e3c
Austin Littley <102969658+alittley@users.noreply.github.com>
2022-10-20 13:01:34 -0400

    Write tests for basic ed25519 signing and verification (#58)
    Signed-off-by: Austin Littley <austin.littley@swirldslabs.com>

---
+++ sdk/tests/ED25519PrivateKeyTest.cpp
@@ -0,0 +1,54 @@
+/*-
+ *
+ * Hedera C++ SDK
+ *
+ * Copyright (C) 2020 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <memory>
+#include <vector>
+
+#include "ED25519PrivateKey.h"
+#include "ED25519PublicKey.h"
+
+TEST(tests, getPublicKey) {
+  std::unique_ptr<Hedera::ED25519PrivateKey> privateKey =
+      std::make_unique<Hedera::ED25519PrivateKey>();
+
+  std::shared_ptr<Hedera::PublicKey> publicKey = privateKey->getPublicKey();
+  EXPECT_NE(publicKey, nullptr);
+}
+
+TEST(tests, sign) {
+  std::unique_ptr<Hedera::ED25519PrivateKey> privateKey =
+      std::make_unique<Hedera::ED25519PrivateKey>();
+
+  std::vector<unsigned char> bytesToSign = {0x1, 0x2, 0x3};
+  std::vector<unsigned char> signature = privateKey->sign(bytesToSign);
+
+  EXPECT_EQ(signature.size(), 64);
+}
+
+TEST(tests, signEmptyBytes) {
+  std::unique_ptr<Hedera::ED25519PrivateKey> privateKey =
+      std::make_unique<Hedera::ED25519PrivateKey>();
+
+  std::vector<unsigned char> signature =
+      privateKey->sign(std::vector<unsigned char>());
+
+  EXPECT_EQ(signature.size(), 64);
+}
