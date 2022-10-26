0166a7930205f6df20922b8db8a6151d3afd902d
Austin Littley <austin.littley@swirldslabs.com>
2022-10-26 14:28:04 -0400

    Address PR comments
    Signed-off-by: Austin Littley <austin.littley@swirldslabs.com>

--- sdk/tests/AccountIdTest.cc
+++ sdk/tests/AccountIdTest.cc
@@ -23,116 +23,109 @@
 
 #include "AccountId.h"
 
+using namespace Hedera;
+
 class AccountIdTest : public ::testing::Test {
 protected:
+  uint64_t realmNumber = 555;
+  uint64_t shardNumber = 666;
+  uint64_t accountNumber = 777;
+
   void SetUp() override {}
 };
 
 TEST_F(AccountIdTest, AccountNumberConstructor) {
-  uint64_t accountNumber = 555;
-
-  std::unique_ptr<Hedera::AccountId> accountId =
-      std::make_unique<Hedera::AccountId>(accountNumber);
+  std::unique_ptr<AccountId> accountId =
+      std::make_unique<AccountId>(accountNumber);
 
   ASSERT_NE(accountId, nullptr);
-  ASSERT_EQ(accountId->getAccountNum(), accountNumber);
-  ASSERT_EQ(accountId->getRealmNum(), 0);
+
   ASSERT_EQ(accountId->getShardNum(), 0);
+  ASSERT_EQ(accountId->getRealmNum(), 0);
+  ASSERT_EQ(accountId->getAccountNum(), accountNumber);
 }
 
 TEST_F(AccountIdTest, RealmShardConstructor) {
-  uint64_t realmNumber = 666;
-  uint64_t shardNumber = 777;
-
-  std::unique_ptr<Hedera::AccountId> accountId =
-      std::make_unique<Hedera::AccountId>(shardNumber, realmNumber);
+  std::unique_ptr<AccountId> accountId =
+      std::make_unique<AccountId>(shardNumber, realmNumber);
 
   ASSERT_NE(accountId, nullptr);
-  ASSERT_FALSE(accountId->getAccountNum().has_value());
-  ASSERT_EQ(accountId->getRealmNum(), realmNumber);
+
   ASSERT_EQ(accountId->getShardNum(), shardNumber);
+  ASSERT_EQ(accountId->getRealmNum(), realmNumber);
+  ASSERT_FALSE(accountId->getAccountNum().has_value());
 }
 
 TEST_F(AccountIdTest, AccountRealmShardConstructor) {
-  uint64_t accountNumber = 555;
-  uint64_t realmNumber = 666;
-  uint64_t shardNumber = 777;
-
-  std::unique_ptr<Hedera::AccountId> accountId =
-      std::make_unique<Hedera::AccountId>(shardNumber, realmNumber,
-                                          accountNumber);
+  std::unique_ptr<AccountId> accountId =
+      std::make_unique<AccountId>(shardNumber, realmNumber, accountNumber);
 
   ASSERT_NE(accountId, nullptr);
-  ASSERT_EQ(accountId->getAccountNum(), accountNumber);
-  ASSERT_EQ(accountId->getRealmNum(), realmNumber);
   ASSERT_EQ(accountId->getShardNum(), shardNumber);
+  ASSERT_EQ(accountId->getRealmNum(), realmNumber);
+  ASSERT_EQ(accountId->getAccountNum(), accountNumber);
 }
 
 TEST_F(AccountIdTest, SetAccountNum) {
-  std::unique_ptr<Hedera::AccountId> accountId =
-      std::make_unique<Hedera::AccountId>(333);
+  std::unique_ptr<AccountId> accountId =
+      std::make_unique<AccountId>(accountNumber);
 
-  uint64_t newAccountNumber = 555;
+  uint64_t newAccountNumber = 11;
   accountId->setAccountNum(newAccountNumber);
 
   ASSERT_EQ(accountId->getAccountNum(), newAccountNumber);
 }
 
 TEST_F(AccountIdTest, SetRealmNum) {
-  std::unique_ptr<Hedera::AccountId> accountId =
-      std::make_unique<Hedera::AccountId>(22, 33, 44);
+  std::unique_ptr<AccountId> accountId =
+      std::make_unique<AccountId>(shardNumber, realmNumber, accountNumber);
 
-  uint64_t newRealmNumber = 55;
+  uint64_t newRealmNumber = 22;
   accountId->setRealmNum(newRealmNumber);
 
   ASSERT_EQ(accountId->getRealmNum(), newRealmNumber);
 }
 
 TEST_F(AccountIdTest, SetShardNum) {
-  std::unique_ptr<Hedera::AccountId> accountId =
-      std::make_unique<Hedera::AccountId>(22, 33, 44);
+  std::unique_ptr<AccountId> accountId =
+      std::make_unique<AccountId>(shardNumber, realmNumber, accountNumber);
 
-  uint64_t newShardNum = 66;
+  uint64_t newShardNum = 33;
   accountId->setShardNum(newShardNum);
 
   ASSERT_EQ(accountId->getShardNum(), newShardNum);
 }
 
 TEST_F(AccountIdTest, Equality) {
-  uint64_t accountNumber = 555;
-  uint64_t realmNumber = 666;
-  uint64_t shardNumber = 777;
-
-  std::unique_ptr<Hedera::AccountId> accountIdOriginal =
-      std::make_unique<Hedera::AccountId>(accountNumber, realmNumber,
-                                          shardNumber);
-  std::unique_ptr<Hedera::AccountId> accountIdDuplicate =
-      std::make_unique<Hedera::AccountId>(accountNumber, realmNumber,
-                                          shardNumber);
+  std::unique_ptr<AccountId> accountIdOriginal =
+      std::make_unique<AccountId>(shardNumber, realmNumber, accountNumber);
+  std::unique_ptr<AccountId> accountIdDuplicate =
+      std::make_unique<AccountId>(shardNumber, realmNumber, accountNumber);
 
   ASSERT_TRUE(*accountIdOriginal.get() == *accountIdDuplicate.get());
 
-  std::unique_ptr<Hedera::AccountId> accountIdDifferentAccount =
-      std::make_unique<Hedera::AccountId>(0, realmNumber, shardNumber);
+  std::unique_ptr<AccountId> accountIdDifferentShard =
+      std::make_unique<AccountId>(0, realmNumber, accountNumber);
 
-  ASSERT_FALSE(*accountIdOriginal.get() == *accountIdDifferentAccount.get());
+  ASSERT_FALSE(*accountIdOriginal.get() == *accountIdDifferentShard.get());
 
-  std::unique_ptr<Hedera::AccountId> accountIdDifferentRealm =
-      std::make_unique<Hedera::AccountId>(accountNumber, 0, shardNumber);
+  std::unique_ptr<AccountId> accountIdDifferentRealm =
+      std::make_unique<AccountId>(shardNumber, 0, accountNumber);
 
   ASSERT_FALSE(*accountIdOriginal.get() == *accountIdDifferentRealm.get());
 
-  std::unique_ptr<Hedera::AccountId> accountIdDifferentShard =
-      std::make_unique<Hedera::AccountId>(accountNumber, realmNumber, 0);
+  std::unique_ptr<AccountId> accountIdDifferentAccount =
+      std::make_unique<AccountId>(shardNumber, realmNumber, 0);
 
-  ASSERT_FALSE(*accountIdOriginal.get() == *accountIdDifferentShard.get());
+  ASSERT_FALSE(*accountIdOriginal.get() == *accountIdDifferentAccount.get());
 }
 
 TEST_F(AccountIdTest, Protobuf) {
-  std::unique_ptr<Hedera::AccountId> accountId =
-      std::make_unique<Hedera::AccountId>(22, 33, 44);
+  std::unique_ptr<AccountId> accountId =
+      std::make_unique<AccountId>(shardNumber, realmNumber, accountNumber);
 
-  Hedera::AccountId accountIdThroughProtobuf = Hedera::AccountId::fromProtobuf(*accountId->toProtobuf());
+  AccountId accountIdThroughProtobuf =
+      AccountId::fromProtobuf(*accountId->toProtobuf());
 
   ASSERT_EQ(*accountId.get(), accountIdThroughProtobuf);
 }
