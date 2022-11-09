b9ff47a243aefd3dd0759ba387925bc2743919ca
Rob Walworth <robert.walworth@swirldslabs.com>
2022-11-09 15:43:21 -0600

    Add TransactionReceiptQueryTest | Fix build issue in TransferCryptoExample
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

---
+++ sdk/tests/TransactionReceiptQueryTest.cc
@@ -0,0 +1,42 @@
+/*-
+ *
+ * Hedera C++ SDK
+ *
+ * Copyright (C) 2020 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include "TransactionReceiptQuery.h"
+#include "TransactionId.h"
+
+#include <gtest/gtest.h>
+
+using namespace Hedera;
+
+class TransactionReceiptQueryTest : public ::testing::Test
+{
+protected:
+  [[nodiscard]] inline const AccountId& getTestAccountId() const { return mTestAccountId; }
+
+private:
+  const AccountId mTestAccountId = AccountId(0ULL, 0ULL, 10ULL);
+};
+
+TEST_F(TransactionReceiptQueryTest, SetTransactionId)
+{
+  TransactionReceiptQuery query;
+  const TransactionId transactionId = TransactionId::generate(getTestAccountId());
+  query.setTransactionId(TransactionId::generate(getTestAccountId()));
+  EXPECT_EQ(query.getTransactionId(), transactionId);
+}
