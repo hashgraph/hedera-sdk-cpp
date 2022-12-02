9c44add7ea0c00f919eebe8d78e731e871f915ef
Nathan Klick <nathan@swirldslabs.com>
2022-12-01 21:44:23 -0600

    Fix HexConverter array initialization compile error under MSVC
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- sdk/main/src/helper/HexConverter.cc
+++ sdk/main/src/helper/HexConverter.cc
@@ -24,55 +24,52 @@
 #include <iostream>
 #include <sstream>
 
-namespace Hedera
-{
-std::string HexConverter::base64ToHex(const std::vector<unsigned char>& bytes)
-{
-  size_t stringLength;
+namespace Hedera {
+    std::string HexConverter::base64ToHex(const std::vector<unsigned char> &bytes) {
+        size_t stringLength;
 
-  if (OPENSSL_buf2hexstr_ex(nullptr, 0, &stringLength, &bytes.front(), bytes.size(), '\0') <= 0)
-  {
-    std::cout << "buf2hexstr_ex determine size error" << std::endl;
-  }
+        if (OPENSSL_buf2hexstr_ex(nullptr, 0, &stringLength, &bytes.front(), bytes.size(), '\0') <= 0) {
+            std::cout << "buf2hexstr_ex determine size error" << std::endl;
+        }
 
-  char charString[stringLength];
+        std::unique_ptr<char> charArray(new char[stringLength]);
+        std::string charString;
+        charString.reserve(stringLength);
 
-  if (OPENSSL_buf2hexstr_ex(charString, stringLength, &stringLength, &bytes.front(), bytes.size(), '\0') <= 0)
-  {
-    std::cout << "buf2hexstr_ex generate string error" << std::endl;
-  }
+        if (OPENSSL_buf2hexstr_ex(charArray.get(), stringLength, &stringLength, &bytes.front(), bytes.size(), '\0') <=
+            0) {
 
-  return { charString };
-}
+            std::cout << "buf2hexstr_ex generate string error" << std::endl;
+        }
 
-std::vector<unsigned char> HexConverter::hexToBase64(const std::string& inputString)
-{
-  size_t bufferLength;
-  if (OPENSSL_hexstr2buf_ex(nullptr, 0, &bufferLength, inputString.c_str(), '\0') <= 0)
-  {
-    std::cout << "hexstr2buf_ex determine size error" << std::endl;
-  }
+        charString.append(charArray.get());
 
-  std::vector<unsigned char> outputBytes = std::vector<unsigned char>(bufferLength);
+        return charString;
+    }
 
-  if (OPENSSL_hexstr2buf_ex(&outputBytes.front(), bufferLength, &bufferLength, inputString.c_str(), '\0') <= 0)
-  {
-    std::cout << "hexstr2buf_ex generate string error" << std::endl;
-  }
+    std::vector<unsigned char> HexConverter::hexToBase64(const std::string &inputString) {
+        size_t bufferLength;
+        if (OPENSSL_hexstr2buf_ex(nullptr, 0, &bufferLength, inputString.c_str(), '\0') <= 0) {
+            std::cout << "hexstr2buf_ex determine size error" << std::endl;
+        }
 
-  return outputBytes;
-}
+        std::vector<unsigned char> outputBytes = std::vector<unsigned char>(bufferLength);
 
-std::string HexConverter::bytesToHex(const std::vector<unsigned char>& bytes)
-{
-  std::stringstream stream;
-  stream << std::hex;
+        if (OPENSSL_hexstr2buf_ex(&outputBytes.front(), bufferLength, &bufferLength, inputString.c_str(), '\0') <= 0) {
+            std::cout << "hexstr2buf_ex generate string error" << std::endl;
+        }
 
-  for (unsigned char byte : bytes)
-  {
-    stream << std::setw(2) << std::setfill('0') << (int)byte;
-  }
+        return outputBytes;
+    }
 
-  return stream.str();
-}
+    std::string HexConverter::bytesToHex(const std::vector<unsigned char> &bytes) {
+        std::stringstream stream;
+        stream << std::hex;
+
+        for (unsigned char byte: bytes) {
+            stream << std::setw(2) << std::setfill('0') << (int) byte;
+        }
+
+        return stream.str();
+    }
 }
