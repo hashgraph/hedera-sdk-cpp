223e9d6c1df610597a5acb5e2bdf89c41453961a
Nathan Klick <nathan@swirldslabs.com>
2022-12-08 11:16:22 -0600

    Fix merge issue with main branch
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- sdk/main/src/impl/HexConverter.cc
+++ sdk/main/src/impl/HexConverter.cc
@@ -24,66 +24,49 @@
 #include <openssl/crypto.h>
 #include <sstream>
 
-namespace Hedera::internal::HexConverter
-{
-std::string base64ToHex(const std::vector<unsigned char>& bytes)
-{
-  size_t stringLength;
+namespace Hedera::internal::HexConverter {
+  std::string base64ToHex(const std::vector<unsigned char> &bytes) {
+    size_t stringLength;
 
     if (OPENSSL_buf2hexstr_ex(nullptr, 0, &stringLength, &bytes.front(), bytes.size(), '\0') <= 0) {
       std::cout << "buf2hexstr_ex determine size error" << std::endl;
     }
 
-    std::unique_ptr<char> charArray(new char[stringLength]);
+    std::string charString(stringLength, '\0');
 
-    if (OPENSSL_buf2hexstr_ex(charArray.get(), stringLength, &stringLength, &bytes.front(), bytes.size(), '\0') <= 0) {
+    if (OPENSSL_buf2hexstr_ex(&charString.front(), stringLength, &stringLength, &bytes.front(), bytes.size(), '\0') <=
+        0) {
       std::cout << "buf2hexstr_ex generate string error" << std::endl;
     }
 
-    std::string charString(charArray.get());
+    charString.pop_back(); // Remove the '\0'
     return charString;
   }
 
-  std::string charString(stringLength, '\0');
+  std::vector<unsigned char> hexToBase64(const std::string &inputString) {
+    size_t bufferLength;
+    if (OPENSSL_hexstr2buf_ex(nullptr, 0, &bufferLength, inputString.c_str(), '\0') <= 0) {
+      std::cout << "hexstr2buf_ex determine size error" << std::endl;
+    }
 
-  if (OPENSSL_buf2hexstr_ex(&charString.front(), stringLength, &stringLength, &bytes.front(), bytes.size(), '\0') <= 0)
-  {
-    std::cout << "buf2hexstr_ex generate string error" << std::endl;
+    std::vector<unsigned char> outputBytes(bufferLength);
+
+    if (OPENSSL_hexstr2buf_ex(&outputBytes.front(), bufferLength, &bufferLength, inputString.c_str(), '\0') <= 0) {
+      std::cout << "hexstr2buf_ex generate string error" << std::endl;
+    }
+
+    return outputBytes;
   }
 
-  charString.pop_back(); // Remove the '\0'
-  return charString;
-}
+  std::string bytesToHex(const std::vector<unsigned char> &bytes) {
+    std::stringstream stream;
+    stream << std::hex;
 
-std::vector<unsigned char> hexToBase64(const std::string& inputString)
-{
-  size_t bufferLength;
-  if (OPENSSL_hexstr2buf_ex(nullptr, 0, &bufferLength, inputString.c_str(), '\0') <= 0)
-  {
-    std::cout << "hexstr2buf_ex determine size error" << std::endl;
+    for (unsigned char byte: bytes) {
+      stream << std::setw(2) << std::setfill('0') << (int) byte;
+    }
+
+    return stream.str();
   }
-
-  std::vector<unsigned char> outputBytes(bufferLength);
-
-  if (OPENSSL_hexstr2buf_ex(&outputBytes.front(), bufferLength, &bufferLength, inputString.c_str(), '\0') <= 0)
-  {
-    std::cout << "hexstr2buf_ex generate string error" << std::endl;
-  }
-
-  return outputBytes;
-}
-
-std::string bytesToHex(const std::vector<unsigned char>& bytes)
-{
-  std::stringstream stream;
-  stream << std::hex;
-
-  for (unsigned char byte : bytes)
-  {
-    stream << std::setw(2) << std::setfill('0') << (int)byte;
-  }
-
-  return stream.str();
-}
 
 } // namespace Hedera::internal::HexConverter
