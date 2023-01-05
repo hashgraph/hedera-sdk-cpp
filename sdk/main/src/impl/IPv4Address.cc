158a94fcac0aaab237e4f42143467501dda5bbc7
Nathan Klick <nathan@swirldslabs.com>
2023-01-05 11:31:19 -0600

    Migrate Actions workflows to new cluster (#155)
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- sdk/main/src/impl/IPv4Address.cc
+++ sdk/main/src/impl/IPv4Address.cc
@@ -19,70 +19,85 @@
  */
 #include "impl/IPv4Address.h"
 
+#include <algorithm>
 #include <charconv>
 #include <iostream>
 #include <sstream>
 #include <vector>
 
-namespace Hedera::internal {
-  IPv4Address::IPv4Address(unsigned char octet1, unsigned char octet2, unsigned char octet3, unsigned char octet4)
-          : mOctet1(octet1), mOctet2(octet2), mOctet3(octet3), mOctet4(octet4) {
+namespace Hedera::internal
+{
+IPv4Address::IPv4Address(unsigned char octet1, unsigned char octet2, unsigned char octet3, unsigned char octet4)
+  : mOctet1(octet1)
+  , mOctet2(octet2)
+  , mOctet3(octet3)
+  , mOctet4(octet4)
+{
+}
+
+IPv4Address IPv4Address::fromString(const std::string& inputString)
+{
+  // input string is in byte format, where each byte represents a single IP address octet
+  if (inputString.size() == 4)
+  {
+    std::vector<unsigned char> byteVector = { inputString.cbegin(), inputString.cend() };
+
+    IPv4Address outputAddress = { byteVector.at(0), byteVector.at(1), byteVector.at(2), byteVector.at(3) };
+
+    return outputAddress;
   }
 
-  IPv4Address IPv4Address::fromString(const std::string &inputString) {
-    // input string is in byte format, where each byte represents a single IP address octet
-    if (inputString.size() == 4) {
-      std::vector<unsigned char> byteVector = {inputString.cbegin(), inputString.cend()};
-
-      IPv4Address outputAddress = {byteVector.at(0), byteVector.at(1), byteVector.at(2), byteVector.at(3)};
-
-      return outputAddress;
-    }
-
-    // if input string isn't bytes, it must be made up of number characters, with 4 octets separated by the '.' character
-    if (std::count(inputString.begin(), inputString.end(), '.') != 3) {
-      throw std::invalid_argument("Input IPv4Address is malformed");
-    }
-
-    std::vector<unsigned char> byteVector;
-
-    int previousDelimiter = -1;
-    int nextDelimiter = (int) inputString.find('.');
-
-    for (int i = 0; i < 4; ++i) {
-      std::string byteString = inputString.substr(previousDelimiter + 1, nextDelimiter - previousDelimiter - 1);
-
-      if (!byteString.length()) {
-        throw std::invalid_argument("Input IPv4Address has empty octet");
-      }
-
-      for (char character: byteString) {
-        if (!std::isdigit(character)) {
-          throw std::invalid_argument("Input IPv4Address has octet that isn't a number");
-        }
-      }
-
-      unsigned int byteInt = std::stoi(byteString);
-      if (byteInt > 255) {
-        throw std::invalid_argument("Input IPv4Address octet is > 255");
-      }
-
-      byteVector.push_back(byteInt);
-
-      previousDelimiter = nextDelimiter;
-      nextDelimiter = (int) inputString.find('.', previousDelimiter + 1);
-    }
-
-    return {byteVector.at(0), byteVector.at(1), byteVector.at(2), byteVector.at(3)};
+  // if input string isn't bytes, it must be made up of number characters, with 4 octets separated by the '.' character
+  if (std::count(inputString.begin(), inputString.end(), '.') != 3)
+  {
+    throw std::invalid_argument("Input IPv4Address is malformed");
   }
 
-  std::string IPv4Address::toString() const {
-    std::stringstream outputStream;
+  std::vector<unsigned char> byteVector;
 
-    outputStream << (unsigned int) mOctet1 << "." << (unsigned int) mOctet2 << "." << (unsigned int) mOctet3 << "."
-                 << (unsigned int) mOctet4;
+  int previousDelimiter = -1;
+  int nextDelimiter = (int)inputString.find('.');
 
-    return outputStream.str();
+  for (int i = 0; i < 4; ++i)
+  {
+    std::string byteString = inputString.substr(previousDelimiter + 1, nextDelimiter - previousDelimiter - 1);
+
+    if (!byteString.length())
+    {
+      throw std::invalid_argument("Input IPv4Address has empty octet");
+    }
+
+    for (char character : byteString)
+    {
+      if (!std::isdigit(character))
+      {
+        throw std::invalid_argument("Input IPv4Address has octet that isn't a number");
+      }
+    }
+
+    unsigned int byteInt = std::stoi(byteString);
+    if (byteInt > 255)
+    {
+      throw std::invalid_argument("Input IPv4Address octet is > 255");
+    }
+
+    byteVector.push_back(byteInt);
+
+    previousDelimiter = nextDelimiter;
+    nextDelimiter = (int)inputString.find('.', previousDelimiter + 1);
   }
+
+  return { byteVector.at(0), byteVector.at(1), byteVector.at(2), byteVector.at(3) };
+}
+
+std::string IPv4Address::toString() const
+{
+  std::stringstream outputStream;
+
+  outputStream << (unsigned int)mOctet1 << "." << (unsigned int)mOctet2 << "." << (unsigned int)mOctet3 << "."
+               << (unsigned int)mOctet4;
+
+  return outputStream.str();
+}
 
 } // namespace Hedera::internal
