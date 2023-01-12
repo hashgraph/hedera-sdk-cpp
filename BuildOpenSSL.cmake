e28eb07c6ae3c4c6644d2cd5e00fccdcf9f7b6b3
Rob Walworth <robert.walworth@swirldslabs.com>
2023-01-12 10:48:55 -0600

    Fix Linux Build
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- BuildOpenSSL.cmake
+++ BuildOpenSSL.cmake
@@ -3,6 +3,13 @@ set(OPENSSL_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/openssl-src) # default 
 set(OPENSSL_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl)
 set(OPENSSL_INCLUDE_DIR ${OPENSSL_INSTALL_DIR}/include)
 set(OPENSSL_CONFIGURE_COMMAND ${OPENSSL_SOURCE_DIR}/Configure)
+
+if (LINUX)
+    set(OPENSSL_BYPRODUCT_DIR ${OPENSSL_INSTALL_DIR}/lib64)
+else ()
+    set(OPENSSL_BYPRODUCT_DIR ${OPENSSL_INSTALL_DIR}/lib)
+endif ()
+
 ExternalProject_Add(OpenSSL
         SOURCE_DIR ${OPENSSL_SOURCE_DIR}
         GIT_REPOSITORY https://github.com/openssl/openssl.git
@@ -16,7 +23,7 @@ ExternalProject_Add(OpenSSL
         TEST_COMMAND ""
         INSTALL_COMMAND make install
         INSTALL_DIR ${OPENSSL_INSTALL_DIR}
-        BUILD_BYPRODUCTS ${OPENSSL_INSTALL_DIR}/lib/libcrypto.a ${OPENSSL_INSTALL_DIR}/lib/libssl.a
+        BUILD_BYPRODUCTS ${OPENSSL_BYPRODUCT_DIR}/libcrypto.a ${OPENSSL_BYPRODUCT_DIR}/libssl.a
         UPDATE_COMMAND ""
         )
 
@@ -24,12 +31,12 @@ ExternalProject_Add(OpenSSL
 # And to please the property INTERFACE_INCLUDE_DIRECTORIES, we make the include directory in advance.
 file(MAKE_DIRECTORY ${OPENSSL_INCLUDE_DIR})
 
-add_library(OpenSSL::SSL STATIC IMPORTED GLOBAL)
-set_property(TARGET OpenSSL::SSL PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libssl.a)
-set_property(TARGET OpenSSL::SSL PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
-add_dependencies(OpenSSL::SSL OpenSSL)
-
 add_library(OpenSSL::Crypto STATIC IMPORTED GLOBAL)
-set_property(TARGET OpenSSL::Crypto PROPERTY IMPORTED_LOCATION ${OPENSSL_INSTALL_DIR}/lib/libcrypto.a)
+set_property(TARGET OpenSSL::Crypto PROPERTY IMPORTED_LOCATION ${OPENSSL_BYPRODUCT_DIR}/libcrypto.a)
 set_property(TARGET OpenSSL::Crypto PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
 add_dependencies(OpenSSL::Crypto OpenSSL)
+
+add_library(OpenSSL::SSL STATIC IMPORTED GLOBAL)
+set_property(TARGET OpenSSL::SSL PROPERTY IMPORTED_LOCATION ${OPENSSL_BYPRODUCT_DIR}/libssl.a)
+set_property(TARGET OpenSSL::SSL PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
+add_dependencies(OpenSSL::SSL OpenSSL)
