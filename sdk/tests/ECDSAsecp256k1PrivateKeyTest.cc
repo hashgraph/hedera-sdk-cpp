513836a8ab6f6e6a77d40cc6e0d7a076b9100030
Rob Walworth <robert.walworth@swirldslabs.com>
2023-02-08 15:41:09 -0600

    Add missing SLIP10 test vector for ECDSAsecp256k1PrivateKey
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- sdk/tests/ECDSAsecp256k1PrivateKeyTest.cc
+++ sdk/tests/ECDSAsecp256k1PrivateKeyTest.cc
@@ -175,3 +175,66 @@ TEST_F(ECDSAsecp256k1PrivateKeyTest, SLIP10TestVector1)
   EXPECT_EQ(privateKey->toBytes(), internal::HexConverter::hexToBase64(expectedPrivate));
   EXPECT_EQ(privateKey->getPublicKey()->toBytes(), internal::HexConverter::hexToBase64(expectedPublic));
 }
+
+TEST_F(ECDSAsecp256k1PrivateKeyTest, SLIP10TestVector2)
+{
+  // SLIP10 spec provided test vector
+  std::string hexSeed = "fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875"
+                        "726f6c696663605d5a5754514e4b484542";
+
+  std::unique_ptr<ECDSAsecp256k1PrivateKey> privateKey =
+    ECDSAsecp256k1PrivateKey::fromSeed(internal::HexConverter::hexToBase64(hexSeed));
+
+  std::string expectedChainCode = "60499f801b896d83179a4374aeb7822aaeaceaa0db1f85ee3e904c4defbd9689";
+  std::string expectedPrivate = "4b03d6fc340455b363f51020ad3ecca4f0850280cf436c70c727923f6db46c3e";
+  std::string expectedPublic = "03cbcaa9c98c877a26977d00825c956a238e8dddfbd322cce4f74b0b5bd6ace4a7";
+
+  EXPECT_EQ(privateKey->getChainCode(), internal::HexConverter::hexToBase64(expectedChainCode));
+  EXPECT_EQ(privateKey->toBytes(), internal::HexConverter::hexToBase64(expectedPrivate));
+  EXPECT_EQ(privateKey->getPublicKey()->toBytes(), internal::HexConverter::hexToBase64(expectedPublic));
+
+  privateKey = privateKey->derive(0);
+  expectedChainCode = "f0909affaa7ee7abe5dd4e100598d4dc53cd709d5a5c2cac40e7412f232f7c9c";
+  expectedPrivate = "abe74a98f6c7eabee0428f53798f0ab8aa1bd37873999041703c742f15ac7e1e";
+  expectedPublic = "02fc9e5af0ac8d9b3cecfe2a888e2117ba3d089d8585886c9c826b6b22a98d12ea";
+
+  EXPECT_EQ(privateKey->getChainCode(), internal::HexConverter::hexToBase64(expectedChainCode));
+  EXPECT_EQ(privateKey->toBytes(), internal::HexConverter::hexToBase64(expectedPrivate));
+  EXPECT_EQ(privateKey->getPublicKey()->toBytes(), internal::HexConverter::hexToBase64(expectedPublic));
+
+  privateKey = privateKey->derive(internal::DerivationPathUtils::getHardenedIndex(2147483647));
+  expectedChainCode = "be17a268474a6bb9c61e1d720cf6215e2a88c5406c4aee7b38547f585c9a37d9";
+  expectedPrivate = "877c779ad9687164e9c2f4f0f4ff0340814392330693ce95a58fe18fd52e6e93";
+  expectedPublic = "03c01e7425647bdefa82b12d9bad5e3e6865bee0502694b94ca58b666abc0a5c3b";
+
+  EXPECT_EQ(privateKey->getChainCode(), internal::HexConverter::hexToBase64(expectedChainCode));
+  EXPECT_EQ(privateKey->toBytes(), internal::HexConverter::hexToBase64(expectedPrivate));
+  EXPECT_EQ(privateKey->getPublicKey()->toBytes(), internal::HexConverter::hexToBase64(expectedPublic));
+
+  privateKey = privateKey->derive(1);
+  expectedChainCode = "f366f48f1ea9f2d1d3fe958c95ca84ea18e4c4ddb9366c336c927eb246fb38cb";
+  expectedPrivate = "704addf544a06e5ee4bea37098463c23613da32020d604506da8c0518e1da4b7";
+  expectedPublic = "03a7d1d856deb74c508e05031f9895dab54626251b3806e16b4bd12e781a7df5b9";
+
+  EXPECT_EQ(privateKey->getChainCode(), internal::HexConverter::hexToBase64(expectedChainCode));
+  EXPECT_EQ(privateKey->toBytes(), internal::HexConverter::hexToBase64(expectedPrivate));
+  EXPECT_EQ(privateKey->getPublicKey()->toBytes(), internal::HexConverter::hexToBase64(expectedPublic));
+
+  privateKey = privateKey->derive(internal::DerivationPathUtils::getHardenedIndex(2147483646));
+  expectedChainCode = "637807030d55d01f9a0cb3a7839515d796bd07706386a6eddf06cc29a65a0e29";
+  expectedPrivate = "f1c7c871a54a804afe328b4c83a1c33b8e5ff48f5087273f04efa83b247d6a2d";
+  expectedPublic = "02d2b36900396c9282fa14628566582f206a5dd0bcc8d5e892611806cafb0301f0";
+
+  EXPECT_EQ(privateKey->getChainCode(), internal::HexConverter::hexToBase64(expectedChainCode));
+  EXPECT_EQ(privateKey->toBytes(), internal::HexConverter::hexToBase64(expectedPrivate));
+  EXPECT_EQ(privateKey->getPublicKey()->toBytes(), internal::HexConverter::hexToBase64(expectedPublic));
+
+  privateKey = privateKey->derive(2);
+  expectedChainCode = "9452b549be8cea3ecb7a84bec10dcfd94afe4d129ebfd3b3cb58eedf394ed271";
+  expectedPrivate = "bb7d39bdb83ecf58f2fd82b6d918341cbef428661ef01ab97c28a4842125ac23";
+  expectedPublic = "024d902e1a2fc7a8755ab5b694c575fce742c48d9ff192e63df5193e4c7afe1f9c";
+
+  EXPECT_EQ(privateKey->getChainCode(), internal::HexConverter::hexToBase64(expectedChainCode));
+  EXPECT_EQ(privateKey->toBytes(), internal::HexConverter::hexToBase64(expectedPrivate));
+  EXPECT_EQ(privateKey->getPublicKey()->toBytes(), internal::HexConverter::hexToBase64(expectedPublic));
+}
