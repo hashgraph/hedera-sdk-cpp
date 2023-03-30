e0cc5e3d7ec3d882f8b5f25efeae0d70353c362f
Deyan Zhekov <deyan.zhekov@limechain.tech>
2023-03-31 00:38:27 +0300

    201: Fixes in ClientIntegrationTest for PR #250.
    Signed-off-by: Deyan Zhekov <deyan.zhekov@limechain.tech>

--- sdk/tests/integration/ClientIntegrationTest.cc
+++ sdk/tests/integration/ClientIntegrationTest.cc
@@ -39,13 +39,23 @@ using namespace Hedera;
 class ClientIntegrationTest : public ::testing::Test
 {
 protected:
-  [[nodiscard]] inline const AccountId& getTestAccountId() const { return mAccountId; }
-  [[nodiscard]] inline const std::unique_ptr<ED25519PrivateKey>& getTestPrivateKey() const { return mPrivateKey; }
+  [[nodiscard]] inline const std::string_view& getJsonNetworkTag() const { return mJsonNetworkTag; }
+  [[nodiscard]] inline const std::string_view& getJsonOperatorIdTag() const { return mJsonOperatorIdTag; }
+  [[nodiscard]] inline const std::string_view& getJsonOperatorPrivateKeyTag() const
+  {
+    return mJsonOperatorPrivateKeyTag;
+  }
+  [[nodiscard]] inline const AccountId& getAccountId() const { return mAccountId; }
+  [[nodiscard]] inline const AccountId& getOperatorAccountId() const { return mOperatorAccountId; }
   [[nodiscard]] inline const std::string getPathToJSON() const { return mFilePath.string(); }
 
 private:
-  const AccountId mAccountId = AccountId(10ULL);
-  const std::unique_ptr<ED25519PrivateKey> mPrivateKey = ED25519PrivateKey::generatePrivateKey();
+  const std::string_view mJsonNetworkTag = "network";
+  const std::string_view mJsonOperatorIdTag = "0.0.2";
+  const std::string_view mJsonOperatorPrivateKeyTag =
+    "302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137";
+  const AccountId mAccountId = AccountId::fromString("0.0.3");
+  const AccountId mOperatorAccountId = AccountId::fromString(mJsonOperatorIdTag);
   const std::filesystem::path mFilePath = std::filesystem::current_path() / "local_node.json";
 };
 
@@ -53,35 +63,35 @@ private:
 TEST_F(ClientIntegrationTest, ConnectToLocalNode)
 {
   // Given
-  const std::string_view testAccountIdStr = "0.0.3";
-  const std::string_view jsonNetworkTag = "network";
-  const std::string_view jsonOperatorIdTag = "0.0.2";
-  const std::string_view jsonOperatorPrivateKey =
-    "302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137";
+  const auto accountId = getAccountId();
+  const std::string_view accountIdStr = accountId.toString();
+  const std::string_view networkTag = getJsonNetworkTag();
+  const std::string_view operatorIdTag = getJsonOperatorIdTag();
+  const std::string_view operatorPrivateKeyTag = getJsonOperatorPrivateKeyTag();
   const std::string testPathToJSON = getPathToJSON();
   const std::unique_ptr<PrivateKey> testPrivateKey = ED25519PrivateKey::generatePrivateKey();
   const std::shared_ptr<PublicKey> testPublicKey = testPrivateKey->getPublicKey();
   const auto testInitialHbarBalance = Hbar(1000ULL, HbarUnit::TINYBAR());
 
   std::ifstream testInputFile(testPathToJSON, std::ios::in);
-  std::string nodeAddress;
+  std::string nodeAddressString;
   json jsonData;
 
   EXPECT_NO_THROW(jsonData = json::parse(testInputFile));
 
-  if (jsonData[jsonNetworkTag][testAccountIdStr].is_string())
+  if (jsonData[networkTag][accountIdStr].is_string())
   {
-    nodeAddress = jsonData[jsonNetworkTag][testAccountIdStr];
+    nodeAddressString = jsonData[networkTag][accountIdStr];
   }
 
   testInputFile.close();
 
   std::unordered_map<std::string, AccountId> networkMap;
-  networkMap.insert(std::pair<std::string, AccountId>(nodeAddress, AccountId::fromString(testAccountIdStr)));
+  networkMap.insert(std::pair<std::string, AccountId>(nodeAddressString, accountId));
 
   // When
   Client client = Client::forNetwork(networkMap);
-  client.setOperator(AccountId::fromString(jsonOperatorIdTag), ED25519PrivateKey::fromString(jsonOperatorPrivateKey));
+  client.setOperator(AccountId::fromString(operatorIdTag), ED25519PrivateKey::fromString(operatorPrivateKeyTag));
 
   TransactionResponse txResp =
     AccountCreateTransaction().setKey(testPublicKey).setInitialBalance(testInitialHbarBalance).execute(client);
@@ -89,7 +99,7 @@ TEST_F(ClientIntegrationTest, ConnectToLocalNode)
   const AccountId newAccountId = txResp.getReceipt(client).getAccountId().value();
 
   // Then
-  EXPECT_EQ(client.getOperatorAccountId()->toString(), jsonOperatorIdTag);
+  EXPECT_EQ(client.getOperatorAccountId()->toString(), operatorIdTag);
   EXPECT_NE(client.getOperatorPublicKey(), nullptr);
   EXPECT_FALSE(newAccountId.toString().empty());
 }
