11863ca457daf8b539f7145698b311174cc4c7d2
Deyan Zhekov <deyan.zhekov@limechain.tech>
2023-04-04 12:16:37 +0300

    164: Fix test AccountBalanceQueryIntegrationTest to use the local node.
    Signed-off-by: Deyan Zhekov <deyan.zhekov@limechain.tech>

--- sdk/tests/integration/AccountBalanceQueryIntegrationTest.cc
+++ sdk/tests/integration/AccountBalanceQueryIntegrationTest.cc
@@ -23,17 +23,85 @@
 #include "Client.h"
 #include "ED25519PrivateKey.h"
 
+#include <filesystem>
+#include <fstream>
 #include <gtest/gtest.h>
+#include <iostream>
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
 using namespace Hedera;
 
 class AccountBalanceQueryIntegrationTest : public ::testing::Test
 {
 protected:
+  [[nodiscard]] inline const std::string_view& getJsonNetworkTag() const { return mJsonNetworkTag; }
+  [[nodiscard]] inline const std::string_view& getJsonOperatorTag() const { return mJsonOperatorTag; }
+  [[nodiscard]] inline const std::string_view& getJsonAccountIdTag() const { return mJsonAccountIdTag; }
+  [[nodiscard]] inline const std::string_view& getJsonPrivateKeyTag() const { return mJsonPrivateKeyTag; }
+
+  [[nodiscard]] inline const std::string_view& getNodeAccountIdStr() const { return mNodeAccountIdStr; }
+  [[nodiscard]] inline const AccountId& getNodeAccountId() const { return mNodeAccountId; }
   [[nodiscard]] inline const AccountId& getTestAccountId() const { return mTestAccountId; }
+  [[nodiscard]] inline const std::string getPathToJSON() const { return mFilePath.string(); }
+
+  [[nodiscard]] inline const Client& getTestClient() const { return mClient; }
+
+  void SetUp() override
+  {
+    const auto accountId = getNodeAccountId();
+    const std::string_view accountIdStr = getNodeAccountIdStr();
+    const std::string_view networkTag = getJsonNetworkTag();
+    const std::string_view operatorTag = getJsonOperatorTag();
+    const std::string_view accountIdTag = getJsonAccountIdTag();
+    const std::string_view privateKeyTag = getJsonPrivateKeyTag();
+
+    const std::string testPathToJSON = getPathToJSON();
+    const std::unique_ptr<PrivateKey> testPrivateKey = ED25519PrivateKey::generatePrivateKey();
+    const std::shared_ptr<PublicKey> testPublicKey = testPrivateKey->getPublicKey();
+
+    AccountId operatorAccountId;
+    std::string operatorAccountPrivateKey;
+    std::ifstream testInputFile(testPathToJSON, std::ios::in);
+    std::string nodeAddressString;
+    json jsonData;
+
+    EXPECT_NO_THROW(jsonData = json::parse(testInputFile));
+
+    if (jsonData[networkTag][accountIdStr].is_string())
+    {
+      nodeAddressString = jsonData[networkTag][accountIdStr];
+    }
+
+    if (jsonData[operatorTag][accountIdTag].is_string() && jsonData[operatorTag][privateKeyTag].is_string())
+    {
+      std::string operatorAccountIdStr = jsonData[operatorTag][accountIdTag];
+
+      operatorAccountId = AccountId::fromString(operatorAccountIdStr);
+      operatorAccountPrivateKey = jsonData[operatorTag][privateKeyTag];
+    }
+
+    testInputFile.close();
+
+    std::unordered_map<std::string, AccountId> networkMap;
+    networkMap.insert(std::pair<std::string, AccountId>(nodeAddressString, accountId));
+
+    mClient = Client::forNetwork(networkMap);
+    mClient.setOperator(operatorAccountId, ED25519PrivateKey::fromString(operatorAccountPrivateKey));
+  }
 
 private:
-  const AccountId mTestAccountId = AccountId(3ULL);
+  const std::string_view mJsonNetworkTag = "network";
+  const std::string_view mJsonOperatorTag = "operator";
+  const std::string_view mJsonAccountIdTag = "accountId";
+  const std::string_view mJsonPrivateKeyTag = "privateKey";
+
+  const std::string_view mNodeAccountIdStr = "0.0.3";
+  const AccountId mNodeAccountId = AccountId::fromString("0.0.3");
+  const AccountId mTestAccountId = AccountId::fromString("0.0.1023");
+  const std::filesystem::path mFilePath = std::filesystem::current_path() / "local_node.json";
+
+  Client mClient;
 };
 
 // Tests invoking of method execute() from AccountBalanceQuery.
@@ -41,14 +109,12 @@ TEST_F(AccountBalanceQueryIntegrationTest, ExecuteRequestToTestnetNode)
 {
   // Given
   const auto testAccountId = getTestAccountId();
-  Client testnetClient = Client::forTestnet();
-  testnetClient.setOperator(getTestAccountId(), ED25519PrivateKey::generatePrivateKey());
 
   AccountBalanceQuery testAccountBalanceQuery;
   testAccountBalanceQuery.setAccountId(testAccountId);
 
   // When
-  const Hbar hbarBalance = testAccountBalanceQuery.execute(testnetClient).getBalance();
+  const Hbar hbarBalance = testAccountBalanceQuery.execute(getTestClient()).getBalance();
 
   // Then
   EXPECT_NE(hbarBalance.toTinybars(), 0);
