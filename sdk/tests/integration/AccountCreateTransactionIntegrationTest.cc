75de6bd4652d9269c064ba476fc4c516ea00480c
Deyan Zhekov <deyan.zhekov@limechain.tech>
2023-04-05 22:13:14 +0300

    164: Resolve comments for PR #197.
    Signed-off-by: Deyan Zhekov <deyan.zhekov@limechain.tech>

--- sdk/tests/integration/AccountCreateTransactionIntegrationTest.cc
+++ sdk/tests/integration/AccountCreateTransactionIntegrationTest.cc
@@ -38,31 +38,22 @@ using namespace Hedera;
 class AccountCreateTransactionIntegrationTest : public ::testing::Test
 {
 protected:
-  [[nodiscard]] inline const std::string_view& getJsonNetworkTag() const { return mJsonNetworkTag; }
-  [[nodiscard]] inline const std::string_view& getJsonOperatorTag() const { return mJsonOperatorTag; }
-  [[nodiscard]] inline const std::string_view& getJsonAccountIdTag() const { return mJsonAccountIdTag; }
-  [[nodiscard]] inline const std::string_view& getJsonPrivateKeyTag() const { return mJsonPrivateKeyTag; }
-
-  [[nodiscard]] inline const std::string_view& getNodeAccountIdStr() const { return mNodeAccountIdStr; }
-  [[nodiscard]] inline const AccountId& getNodeAccountId() const { return mNodeAccountId; }
   [[nodiscard]] inline const AccountId& getTestAccountId() const { return mTestAccountId; }
-  [[nodiscard]] inline const std::string getPathToJSON() const { return mFilePath.string(); }
-
-  [[nodiscard]] inline const Client& getTestClient() const { return mClient; }
-
   [[nodiscard]] inline const std::shared_ptr<PublicKey>& getTestPublicKey() const { return mPublicKey; }
   [[nodiscard]] inline const Hbar& getTestBalance() const { return mTestBalance; }
 
+  [[nodiscard]] inline const Client& getTestClient() const { return mClient; }
+
   void SetUp() override
   {
-    const auto accountId = getNodeAccountId();
-    const std::string_view accountIdStr = getNodeAccountIdStr();
-    const std::string_view networkTag = getJsonNetworkTag();
-    const std::string_view operatorTag = getJsonOperatorTag();
-    const std::string_view accountIdTag = getJsonAccountIdTag();
-    const std::string_view privateKeyTag = getJsonPrivateKeyTag();
+    const auto accountId = AccountId::fromString("0.0.3");
+    const std::string_view accountIdStr = "0.0.3";
+    const std::string_view networkTag = "network";
+    const std::string_view operatorTag = "operator";
+    const std::string_view accountIdTag = "accountId";
+    const std::string_view privateKeyTag = "privateKey";
 
-    const std::string testPathToJSON = getPathToJSON();
+    const std::string testPathToJSON = std::filesystem::current_path() / "local_node.json";
     const std::unique_ptr<PrivateKey> testPrivateKey = ED25519PrivateKey::generatePrivateKey();
     const std::shared_ptr<PublicKey> testPublicKey = testPrivateKey->getPublicKey();
 
@@ -95,16 +86,7 @@ protected:
   }
 
 private:
-  const std::string_view mJsonNetworkTag = "network";
-  const std::string_view mJsonOperatorTag = "operator";
-  const std::string_view mJsonAccountIdTag = "accountId";
-  const std::string_view mJsonPrivateKeyTag = "privateKey";
-
-  const std::string_view mNodeAccountIdStr = "0.0.3";
-  const AccountId mNodeAccountId = AccountId::fromString("0.0.3");
   const AccountId mTestAccountId = AccountId::fromString("0.0.1023");
-  const std::filesystem::path mFilePath = std::filesystem::current_path() / "local_node.json";
-
   const std::shared_ptr<PublicKey> mPublicKey = ED25519PrivateKey::generatePrivateKey()->getPublicKey();
   const Hbar mTestBalance = Hbar(1000ULL, HbarUnit::TINYBAR());
 
