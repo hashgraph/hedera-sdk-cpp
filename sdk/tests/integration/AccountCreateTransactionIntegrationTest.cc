c26e67b8d14fbddbc5d14ce9f8790feb0eff804f
Deyan Zhekov <deyan.zhekov@limechain.tech>
2023-04-21 16:10:33 +0300

    197: Add new test to AccountCreateTransactionIntegrationTest.
    Signed-off-by: Deyan Zhekov <deyan.zhekov@limechain.tech>

--- sdk/tests/integration/AccountCreateTransactionIntegrationTest.cc
+++ sdk/tests/integration/AccountCreateTransactionIntegrationTest.cc
@@ -25,12 +25,15 @@
 #include "TransactionReceipt.h"
 #include "TransactionRecord.h"
 #include "TransactionResponse.h"
+#include "impl/DurationConverter.h"
+#include "impl/Utilities.h"
 
 #include <filesystem>
 #include <fstream>
 #include <gtest/gtest.h>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <proto/transaction_body.pb.h>
 
 using json = nlohmann::json;
 using namespace Hedera;
@@ -38,9 +41,15 @@ using namespace Hedera;
 class AccountCreateTransactionIntegrationTest : public ::testing::Test
 {
 protected:
-  [[nodiscard]] inline const AccountId& getTestAccountId() const { return mTestAccountId; }
   [[nodiscard]] inline const std::shared_ptr<PublicKey>& getTestPublicKey() const { return mPublicKey; }
-  [[nodiscard]] inline const Hbar& getTestBalance() const { return mTestBalance; }
+  [[nodiscard]] inline const Hbar& getTestInitialBalance() const { return mInitialBalance; }
+  [[nodiscard]] inline bool getTestReceiverSignatureRequired() const { return mReceiverSignatureRequired; }
+  [[nodiscard]] inline const std::chrono::duration<double>& getTestAutoRenewPeriod() const { return mAutoRenewPeriod; }
+  [[nodiscard]] inline const std::string& getTestAccountMemo() const { return mAccountMemo; }
+  [[nodiscard]] inline uint32_t getTestMaximumTokenAssociations() const { return mMaxTokenAssociations; }
+  [[nodiscard]] inline const AccountId& getTestAccountId() const { return mAccountId; }
+  [[nodiscard]] inline bool getTestDeclineStakingReward() const { return mDeclineStakingReward; }
+  // [[nodiscard]] inline const EvmAddress& getTestEvmAddress() const { return mEvmAddress; }
 
   [[nodiscard]] inline const Client& getTestClient() const { return mClient; }
 
@@ -86,9 +95,15 @@ protected:
   }
 
 private:
-  const AccountId mTestAccountId = AccountId::fromString("0.0.1023");
   const std::shared_ptr<PublicKey> mPublicKey = ED25519PrivateKey::generatePrivateKey()->getPublicKey();
-  const Hbar mTestBalance = Hbar(1000ULL, HbarUnit::TINYBAR());
+  const Hbar mInitialBalance = Hbar(1000ULL, HbarUnit::TINYBAR());
+  const bool mReceiverSignatureRequired = true;
+  const std::chrono::duration<double> mAutoRenewPeriod = std::chrono::hours(3);
+  const std::string mAccountMemo = "Test Account Memo";
+  const uint32_t mMaxTokenAssociations = 3U;
+  const AccountId mAccountId = AccountId::fromString("0.0.1023");
+  const bool mDeclineStakingReward = true;
+  // const EvmAddress mEvmAddress = EvmAddress::fromString("303132333435363738396162636465666768696a");
 
   Client mClient;
 };
@@ -97,7 +112,7 @@ private:
 TEST_F(AccountCreateTransactionIntegrationTest, ExecuteRequestToLocalNode)
 {
   // Given
-  const auto testBalance = getTestBalance();
+  const auto testBalance = getTestInitialBalance();
   const auto testPublicKey = getTestPublicKey();
   const auto testMemo = "Test memo for AccountCreateTransaction.";
 
@@ -119,4 +134,50 @@ TEST_F(AccountCreateTransactionIntegrationTest, ExecuteRequestToLocalNode)
   EXPECT_TRUE(txRecord.getTransactionId().has_value());
   EXPECT_EQ(txRecord.getTransactionMemo(), testMemo);
   EXPECT_TRUE(txRecord.getNftTransferList().empty());
+}
+
+// Tests invoking of method build() with protobuf object CryptoCreateTransactionBody
+TEST_F(AccountCreateTransactionIntegrationTest, ConstructAccountCreateTransactionFromTransactionBodyProtobuf)
+{
+  // Given
+  const auto testPublicKey = getTestPublicKey();
+  const auto testInitialBalance = getTestInitialBalance();
+  const auto testReceiverSignatureRequired = getTestReceiverSignatureRequired();
+  const auto testAutoRenewPeriod = getTestAutoRenewPeriod();
+  const auto testAccountMemo = getTestAccountMemo();
+  const auto testMaxTokenAssociations = getTestMaximumTokenAssociations();
+  const auto testAccount = getTestAccountId();
+  const auto testDeclineStakingReward = getTestDeclineStakingReward();
+  const std::vector<std::byte> testPublicKeyBytes = testPublicKey->toBytesDer();
+
+  auto body = std::make_unique<proto::CryptoCreateTransactionBody>();
+  body->set_allocated_key(testPublicKey->toProtobuf().release());
+  body->set_initialbalance(static_cast<uint64_t>(testInitialBalance.toTinybars()));
+  body->set_receiversigrequired(testReceiverSignatureRequired);
+  body->set_allocated_autorenewperiod(internal::DurationConverter::toProtobuf(testAutoRenewPeriod));
+  body->set_allocated_memo(new std::string(testAccountMemo));
+  body->set_max_automatic_token_associations(static_cast<int32_t>(testMaxTokenAssociations));
+  body->set_allocated_staked_account_id(testAccount.toProtobuf().release());
+  body->set_decline_reward(testDeclineStakingReward);
+  body->set_allocated_alias(new std::string(internal::Utilities::byteVectorToString(testPublicKeyBytes)));
+
+  proto::TransactionBody txBody;
+  txBody.set_allocated_cryptocreateaccount(body.release());
+
+  // When
+  AccountCreateTransaction accountCreateTransaction(txBody);
+
+  // Then
+  EXPECT_EQ(accountCreateTransaction.getKey()->toStringDer(), testPublicKey->toStringDer());
+  EXPECT_EQ(accountCreateTransaction.getInitialBalance(), testInitialBalance);
+  EXPECT_EQ(accountCreateTransaction.getReceiverSignatureRequired(), testReceiverSignatureRequired);
+  EXPECT_EQ(accountCreateTransaction.getAutoRenewPeriod(), testAutoRenewPeriod);
+  EXPECT_EQ(accountCreateTransaction.getAccountMemo(), testAccountMemo);
+  EXPECT_EQ(accountCreateTransaction.getMaxAutomaticTokenAssociations(), testMaxTokenAssociations);
+  ASSERT_TRUE(accountCreateTransaction.getStakedAccountId().has_value());
+  EXPECT_EQ(accountCreateTransaction.getStakedAccountId(), testAccount);
+  EXPECT_FALSE(accountCreateTransaction.getStakedNodeId().has_value());
+  EXPECT_EQ(accountCreateTransaction.getDeclineStakingReward(), testDeclineStakingReward);
+  EXPECT_EQ(accountCreateTransaction.getPublicKeyAlias()->toBytesDer(), testPublicKeyBytes);
+  EXPECT_FALSE(accountCreateTransaction.getEvmAddressAlias().has_value());
 }
