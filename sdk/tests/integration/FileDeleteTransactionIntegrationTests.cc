42965f99697aa37a61da4a42da7fb7553ff0c54e
Rob Walworth <robert.walworth@swirldslabs.com>
2023-05-04 13:30:30 -0500

    Add FileDeleteTransactionIntegrationTests
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

---
+++ sdk/tests/integration/FileDeleteTransactionIntegrationTests.cc
@@ -0,0 +1,135 @@
+/*-
+ *
+ * Hedera C++ SDK
+ *
+ * Copyright (C) 2020 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include "AccountId.h"
+#include "Client.h"
+#include "ED25519PrivateKey.h"
+#include "FileCreateTransaction.h"
+#include "FileDeleteTransaction.h"
+#include "PrivateKey.h"
+#include "PublicKey.h"
+#include "TransactionReceipt.h"
+#include "TransactionResponse.h"
+#include "exceptions/ReceiptStatusException.h"
+
+#include <filesystem>
+#include <fstream>
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+using json = nlohmann::json;
+using namespace Hedera;
+
+class FileDeleteTransactionIntegrationTest : public ::testing::Test
+{
+protected:
+  [[nodiscard]] inline const Client& getTestClient() const { return mClient; }
+
+  void SetUp() override
+  {
+    const auto accountId = AccountId::fromString("0.0.3");
+    const std::string_view accountIdStr = "0.0.3";
+    const std::string_view networkTag = "network";
+    const std::string_view operatorTag = "operator";
+    const std::string_view accountIdTag = "accountId";
+    const std::string_view privateKeyTag = "privateKey";
+
+    const std::string testPathToJSON = (std::filesystem::current_path() / "local_node.json").string();
+    const std::unique_ptr<PrivateKey> testPrivateKey = ED25519PrivateKey::generatePrivateKey();
+    const std::shared_ptr<PublicKey> testPublicKey = testPrivateKey->getPublicKey();
+
+    AccountId operatorAccountId;
+    std::string operatorAccountPrivateKey;
+    std::ifstream testInputFile(testPathToJSON, std::ios::in);
+    std::string nodeAddressString;
+    json jsonData = json::parse(testInputFile);
+
+    if (jsonData[networkTag][accountIdStr].is_string())
+    {
+      nodeAddressString = jsonData[networkTag][accountIdStr];
+    }
+
+    if (jsonData[operatorTag][accountIdTag].is_string() && jsonData[operatorTag][privateKeyTag].is_string())
+    {
+      std::string operatorAccountIdStr = jsonData[operatorTag][accountIdTag];
+
+      operatorAccountId = AccountId::fromString(operatorAccountIdStr);
+      operatorAccountPrivateKey = jsonData[operatorTag][privateKeyTag];
+    }
+
+    testInputFile.close();
+
+    std::unordered_map<std::string, AccountId> networkMap;
+    networkMap.try_emplace(nodeAddressString, accountId);
+
+    mClient = Client::forNetwork(networkMap);
+    mClient.setOperator(operatorAccountId, ED25519PrivateKey::fromString(operatorAccountPrivateKey));
+  }
+
+private:
+  Client mClient;
+};
+
+//-----
+TEST_F(FileDeleteTransactionIntegrationTest, ExecuteFileDeleteTransaction)
+{
+  // Given
+  const std::unique_ptr<PrivateKey> operatorKey = ED25519PrivateKey::fromString(
+    "302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137");
+  FileId fileId;
+  ASSERT_NO_THROW(fileId = FileCreateTransaction()
+                             .setKey(operatorKey->getPublicKey())
+                             .setContents({})
+                             .execute(getTestClient())
+                             .getReceipt(getTestClient())
+                             .getFileId()
+                             .value());
+
+  // When / Then
+  EXPECT_NO_THROW(const TransactionReceipt txReceipt =
+                    FileDeleteTransaction().setFileId(fileId).execute(getTestClient()).getReceipt(getTestClient()));
+  // TODO: FileInfoQuery
+}
+
+//-----
+TEST_F(FileDeleteTransactionIntegrationTest, CannotDeleteFileWithNoAdminKey)
+{
+  // Given
+  FileId fileId;
+  ASSERT_NO_THROW(
+    fileId =
+      FileCreateTransaction().setContents({}).execute(getTestClient()).getReceipt(getTestClient()).getFileId().value());
+
+  // When / Then
+  EXPECT_THROW(const TransactionReceipt txReceipt =
+                 FileDeleteTransaction().setFileId(fileId).execute(getTestClient()).getReceipt(getTestClient()),
+               ReceiptStatusException); // UNAUTHORIZED
+}
+
+//-----
+TEST_F(FileDeleteTransactionIntegrationTest, CannotDeleteFileWithoutFileId)
+{
+  // Given / When / Then
+  EXPECT_THROW(const TransactionReceipt txReceipt =
+                 FileDeleteTransaction().execute(getTestClient()).getReceipt(getTestClient()),
+               ReceiptStatusException); // INVALID_FILE_ID
+}
