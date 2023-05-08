ff12e6e5c714162734c6645863465f90e83687d7
Rob Walworth <robert.walworth@swirldslabs.com>
2023-05-08 15:22:16 -0500

    Add ContractUpdateTransactionIntegrationTests
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- sdk/main/src/impl/Node.cc
+++ sdk/main/src/impl/Node.cc
@@ -149,6 +149,8 @@ grpc::Status Node::submitTransaction(proto::TransactionBody::DataCase funcEnum,
       return mSmartContractStub->createContract(&context, transaction, response);
     case proto::TransactionBody::DataCase::kContractDeleteInstance:
       return mSmartContractStub->deleteContract(&context, transaction, response);
+    case proto::TransactionBody::DataCase::kContractUpdateInstance:
+      return mSmartContractStub->updateContract(&context, transaction, response);
     case proto::TransactionBody::DataCase::kCryptoAddLiveHash:
       return mCryptoStub->addLiveHash(&context, transaction, response);
     case proto::TransactionBody::DataCase::kCryptoApproveAllowance:
