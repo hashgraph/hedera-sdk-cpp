170cf44896f200a52ceb7e766663c94d96d2e46d
Deyan Zhekov <deyan.zhekov@limechain.tech>
2023-05-30 20:46:20 +0300

    Better coverage in the integration tests (PR) (#297)
    Signed-off-by: Deyan Zhekov <deyan.zhekov@limechain.tech>
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>
    Co-authored-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- sdk/tests/integration/TransactionRecordQueryIntegrationTest.cc
+++ sdk/tests/integration/TransactionRecordQueryIntegrationTest.cc
@@ -18,19 +18,26 @@
  *
  */
 #include "AccountCreateTransaction.h"
+#include "AccountDeleteTransaction.h"
 #include "Client.h"
+#include "ContractCreateTransaction.h"
+#include "ContractDeleteTransaction.h"
 #include "ED25519PrivateKey.h"
+#include "FileCreateTransaction.h"
+#include "FileDeleteTransaction.h"
 #include "TransactionId.h"
 #include "TransactionReceipt.h"
 #include "TransactionRecord.h"
 #include "TransactionRecordQuery.h"
 #include "TransactionResponse.h"
+#include "exceptions/PrecheckStatusException.h"
+#include "impl/Utilities.h"
 
 #include <filesystem>
 #include <fstream>
 #include <gtest/gtest.h>
-#include <iostream>
 #include <nlohmann/json.hpp>
+#include <proto/transaction_body.pb.h>
 
 using json = nlohmann::json;
 using namespace Hedera;
@@ -39,6 +46,7 @@ class TransactionRecordQueryIntegrationTest : public ::testing::Test
 {
 protected:
   [[nodiscard]] inline const Client& getTestClient() const { return mClient; }
+  [[nodiscard]] inline const AccountId& getTestAccountId() const { return mTestAccountId; }
 
   void SetUp() override
   {
@@ -83,27 +91,32 @@ protected:
 
 private:
   Client mClient;
+
+  const AccountId mTestAccountId = AccountId::fromString("0.0.1023");
 };
 
-// Tests invoking of method execute() from TransactionRecordQuery.
-TEST_F(TransactionRecordQueryIntegrationTest, ExecuteRequestToTestnetNode)
+//-----
+TEST_F(TransactionRecordQueryIntegrationTest, CanGetTransactionRecord)
 {
   // Given
-  const auto testMemo = "Test memo for TransactionRecordQuery.";
-  const auto testPublicKey = ED25519PrivateKey::generatePrivateKey()->getPublicKey();
+  const std::unique_ptr<PrivateKey> testPrivateKey = ED25519PrivateKey::generatePrivateKey();
+  const std::shared_ptr<PublicKey> testPublicKey = testPrivateKey->getPublicKey();
 
-  AccountCreateTransaction testAccountCreateTransaction;
-  testAccountCreateTransaction.setKey(testPublicKey.get());
-  testAccountCreateTransaction.setTransactionMemo(testMemo);
+  TransactionResponse testTxResponse;
+  ASSERT_NO_THROW(testTxResponse = AccountCreateTransaction().setKey(testPublicKey.get()).execute(getTestClient()));
 
-  const TransactionResponse txResponse = testAccountCreateTransaction.execute(getTestClient());
+  // When / Then
+  TransactionRecord txRecord;
+  EXPECT_NO_THROW(
+    txRecord = TransactionRecordQuery().setTransactionId(testTxResponse.getTransactionId()).execute(getTestClient()));
 
-  // When
-  const TransactionRecord txRecord = txResponse.getRecord(getTestClient());
-
-  // Then
-  EXPECT_TRUE(txRecord.getReceipt().has_value());
-  EXPECT_TRUE(txRecord.getConsensusTimestamp().has_value());
-  EXPECT_EQ(txRecord.getReceipt()->getStatus(), Status::SUCCESS);
-  EXPECT_EQ(txRecord.getTransactionMemo(), testMemo);
+  // Clean up
+  AccountId accountId;
+  ASSERT_NO_THROW(accountId = txRecord.getReceipt()->getAccountId().value());
+  ASSERT_NO_THROW(AccountDeleteTransaction()
+                    .setDeleteAccountId(accountId)
+                    .setTransferAccountId(AccountId(2ULL))
+                    .freezeWith(getTestClient())
+                    .sign(testPrivateKey.get())
+                    .execute(getTestClient()));
 }
