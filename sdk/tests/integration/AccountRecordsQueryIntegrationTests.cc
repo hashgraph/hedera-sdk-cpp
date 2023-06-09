4d6de816da06fa58e820f121ab3ca0a8d14fb2df
Deyan Zhekov <deyan.zhekov@limechain.tech>
2023-06-09 19:58:25 +0300

    284: Common logic to setup the local node for the integration tests
    Signed-off-by: Deyan Zhekov <deyan.zhekov@limechain.tech>

--- sdk/tests/integration/AccountRecordsQueryIntegrationTests.cc
+++ sdk/tests/integration/AccountRecordsQueryIntegrationTests.cc
@@ -22,6 +22,7 @@
 #include "AccountId.h"
 #include "AccountRecords.h"
 #include "AccountRecordsQuery.h"
+#include "BaseIntegrationTest.h"
 #include "Client.h"
 #include "Defaults.h"
 #include "ED25519PrivateKey.h"
@@ -32,65 +33,12 @@
 #include "TransferTransaction.h"
 #include "exceptions/PrecheckStatusException.h"
 
-#include <filesystem>
-#include <fstream>
 #include <gtest/gtest.h>
-#include <nlohmann/json.hpp>
-#include <string>
-#include <string_view>
-#include <unordered_map>
 
-using json = nlohmann::json;
 using namespace Hedera;
 
-class AccountRecordsQueryIntegrationTest : public ::testing::Test
+class AccountRecordsQueryIntegrationTest : public BaseIntegrationTest
 {
-protected:
-  [[nodiscard]] inline const Client& getTestClient() const { return mClient; }
-
-  void SetUp() override
-  {
-    const auto accountId = AccountId::fromString("0.0.3");
-    const std::string_view accountIdStr = "0.0.3";
-    const std::string_view networkTag = "network";
-    const std::string_view operatorTag = "operator";
-    const std::string_view accountIdTag = "accountId";
-    const std::string_view privateKeyTag = "privateKey";
-
-    const std::string testPathToJSON = (std::filesystem::current_path() / "local_node.json").string();
-    const std::unique_ptr<PrivateKey> testPrivateKey = ED25519PrivateKey::generatePrivateKey();
-    const std::shared_ptr<PublicKey> testPublicKey = testPrivateKey->getPublicKey();
-
-    AccountId operatorAccountId;
-    std::string operatorAccountPrivateKey;
-    std::ifstream testInputFile(testPathToJSON, std::ios::in);
-    std::string nodeAddressString;
-    json jsonData = json::parse(testInputFile);
-
-    if (jsonData[networkTag][accountIdStr].is_string())
-    {
-      nodeAddressString = jsonData[networkTag][accountIdStr];
-    }
-
-    if (jsonData[operatorTag][accountIdTag].is_string() && jsonData[operatorTag][privateKeyTag].is_string())
-    {
-      std::string operatorAccountIdStr = jsonData[operatorTag][accountIdTag];
-
-      operatorAccountId = AccountId::fromString(operatorAccountIdStr);
-      operatorAccountPrivateKey = jsonData[operatorTag][privateKeyTag];
-    }
-
-    testInputFile.close();
-
-    std::unordered_map<std::string, AccountId> networkMap;
-    networkMap.try_emplace(nodeAddressString, accountId);
-
-    mClient = Client::forNetwork(networkMap);
-    mClient.setOperator(operatorAccountId, ED25519PrivateKey::fromString(operatorAccountPrivateKey).get());
-  }
-
-private:
-  Client mClient;
 };
 
 //-----
