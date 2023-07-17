58c7c8862f627e9e0d9a6c5a830fd8988f7d9a1b
Rob Walworth <robert.walworth@swirldslabs.com>
2023-07-17 14:49:36 -0500

    Add TopicCreateTransaction integration tests | Add consensus service stub to Node
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- sdk/main/src/impl/Node.cc
+++ sdk/main/src/impl/Node.cc
@@ -66,6 +66,11 @@ bool Node::connect(const std::chrono::system_clock::time_point& timeout)
 //-----
 void Node::shutdown()
 {
+  if (mConsensusStub)
+  {
+    mConsensusStub.reset();
+  }
+
   if (mCryptoStub)
   {
     mCryptoStub.reset();
@@ -158,6 +163,8 @@ grpc::Status Node::submitTransaction(proto::TransactionBody::DataCase funcEnum,
 
   switch (funcEnum)
   {
+    case proto::TransactionBody::DataCase::kConsensusCreateTopic:
+      return mConsensusStub->createTopic(&context, transaction, response);
     case proto::TransactionBody::DataCase::kContractCall:
       return mSmartContractStub->contractCallMethod(&context, transaction, response);
     case proto::TransactionBody::DataCase::kContractCreateInstance:
@@ -329,6 +336,7 @@ bool Node::initializeChannel(const std::chrono::system_clock::time_point& deadli
 
       if (mChannel->WaitForConnected(deadline))
       {
+        mConsensusStub = proto::ConsensusService::NewStub(mChannel);
         mCryptoStub = proto::CryptoService::NewStub(mChannel);
         mFileStub = proto::FileService::NewStub(mChannel);
         mSmartContractStub = proto::SmartContractService::NewStub(mChannel);
