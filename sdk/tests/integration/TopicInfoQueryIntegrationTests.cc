926f4e3508453d5c8b634cfa0d6571e18bc02c51
Rob Walworth <robert.walworth@swirldslabs.com>
2023-07-20 16:49:42 -0500

    Add TopicInfoQuery integration tests
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

---
+++ sdk/tests/integration/TopicInfoQueryIntegrationTests.cc
@@ -0,0 +1,83 @@
+/*-
+ *
+ * Hedera C++ SDK
+ *
+ * Copyright (C) 2020 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include "BaseIntegrationTest.h"
+#include "Client.h"
+#include "CustomFixedFee.h"
+#include "ED25519PrivateKey.h"
+#include "PrivateKey.h"
+#include "TopicCreateTransaction.h"
+#include "TopicDeleteTransaction.h"
+#include "TopicInfo.h"
+#include "TopicInfoQuery.h"
+#include "TransactionReceipt.h"
+#include "TransactionResponse.h"
+#include "impl/Utilities.h"
+
+#include <gtest/gtest.h>
+
+using namespace Hedera;
+
+class TopicInfoQueryIntegrationTest : public BaseIntegrationTest
+{
+};
+
+//-----
+TEST_F(TopicInfoQueryIntegrationTest, ExecuteTopicInfoQuery)
+{
+  // Given
+  const std::string memo = "test memo";
+  const std::chrono::duration<double> autoRenewPeriod = std::chrono::hours(2200);
+
+  std::shared_ptr<PrivateKey> operatorKey;
+  ASSERT_NO_THROW(
+    operatorKey = ED25519PrivateKey::fromString(
+      "302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137"));
+
+  TopicId topicId;
+  ASSERT_NO_THROW(topicId = TopicCreateTransaction()
+                              .setMemo(memo)
+                              .setAdminKey(operatorKey)
+                              .setSubmitKey(operatorKey)
+                              .setAutoRenewPeriod(autoRenewPeriod)
+                              .setAutoRenewAccountId(AccountId(2ULL))
+                              .execute(getTestClient())
+                              .getReceipt(getTestClient())
+                              .mTopicId.value());
+
+  // When
+  TopicInfo topicInfo;
+  EXPECT_NO_THROW(topicInfo = TopicInfoQuery().setTopicId(topicId).execute(getTestClient()));
+
+  // Then
+  EXPECT_EQ(topicInfo.mTopicId, topicId);
+  EXPECT_EQ(topicInfo.mMemo, memo);
+  ASSERT_NE(topicInfo.mAdminKey, nullptr);
+  EXPECT_EQ(topicInfo.mAdminKey->toBytes(), operatorKey->getPublicKey()->toBytes());
+  ASSERT_NE(topicInfo.mSubmitKey, nullptr);
+  EXPECT_EQ(topicInfo.mSubmitKey->toBytes(), operatorKey->getPublicKey()->toBytes());
+  ASSERT_TRUE(topicInfo.mAutoRenewPeriod.has_value());
+  EXPECT_EQ(topicInfo.mAutoRenewPeriod.value(), autoRenewPeriod);
+  ASSERT_TRUE(topicInfo.mAutoRenewAccountId.has_value());
+  EXPECT_EQ(topicInfo.mAutoRenewAccountId.value(), AccountId(2ULL));
+
+  // Clean up
+  ASSERT_NO_THROW(const TransactionReceipt txReceipt =
+                    TopicDeleteTransaction().setTopicId(topicId).execute(getTestClient()).getReceipt(getTestClient()));
+}
