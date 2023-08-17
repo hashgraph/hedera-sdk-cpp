936bdb1730036ade482540352590ba5d25b83eb2
Rob Walworth <robert.walworth@swirldslabs.com>
2023-08-17 14:07:34 -0500

    Add unit tests for ScheduleInfoQuery and ScheduleInfo
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- sdk/main/src/Query.cc
+++ sdk/main/src/Query.cc
@@ -29,6 +29,8 @@
 #include "FileContentsQuery.h"
 #include "FileInfo.h"
 #include "FileInfoQuery.h"
+#include "ScheduleInfo.h"
+#include "ScheduleInfoQuery.h"
 #include "TokenInfo.h"
 #include "TokenInfoQuery.h"
 #include "TokenNftInfo.h"
@@ -51,6 +53,7 @@ template class Query<ContractCallQuery, ContractFunctionResult>;
 template class Query<ContractInfoQuery, ContractInfo>;
 template class Query<FileContentsQuery, FileContents>;
 template class Query<FileInfoQuery, FileInfo>;
+template class Query<ScheduleInfoQuery, ScheduleInfo>;
 template class Query<TokenInfoQuery, TokenInfo>;
 template class Query<TokenNftInfoQuery, TokenNftInfo>;
 template class Query<TopicInfoQuery, TopicInfo>;
