118e60fdef8e00ac69080b8f3bd39963bcd58a57
Rob Walworth <robert.walworth@swirldslabs.com>
2023-08-25 15:57:18 -0500

    Add SystemUndeleteTransaction
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

---
+++ sdk/main/src/SystemUndeleteTransaction.cc
@@ -0,0 +1,110 @@
+/*-
+ *
+ * Hedera C++ SDK
+ *
+ * Copyright (C) 2020 - 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include "SystemUndeleteTransaction.h"
+#include "impl/Node.h"
+#include "impl/TimestampConverter.h"
+
+#include <grpcpp/client_context.h>
+#include <proto/system_undelete.pb.h>
+#include <proto/transaction.pb.h>
+#include <stdexcept>
+
+namespace Hedera
+{
+//-----
+SystemUndeleteTransaction::SystemUndeleteTransaction(const proto::TransactionBody& transactionBody)
+  : Transaction<SystemUndeleteTransaction>(transactionBody)
+{
+  if (!transactionBody.has_systemundelete())
+  {
+    throw std::invalid_argument("Transaction body doesn't contain SystemUndelete data");
+  }
+
+  const proto::SystemUndeleteTransactionBody& body = transactionBody.systemundelete();
+
+  if (body.has_fileid())
+  {
+    mFileId = FileId::fromProtobuf(body.fileid());
+  }
+  else if (body.has_contractid())
+  {
+    mContractId = ContractId::fromProtobuf(body.contractid());
+  }
+}
+
+//-----
+SystemUndeleteTransaction& SystemUndeleteTransaction::setFileId(const FileId& fileId)
+{
+  requireNotFrozen();
+  mFileId = fileId;
+  mContractId.reset();
+  return *this;
+}
+
+//-----
+SystemUndeleteTransaction& SystemUndeleteTransaction::setContractId(const ContractId& contractId)
+{
+  requireNotFrozen();
+  mContractId = contractId;
+  mFileId.reset();
+  return *this;
+}
+
+//-----
+proto::Transaction SystemUndeleteTransaction::makeRequest(const Client& client,
+                                                          const std::shared_ptr<internal::Node>&) const
+{
+  return signTransaction(generateTransactionBody(&client), client);
+}
+
+//-----
+grpc::Status SystemUndeleteTransaction::submitRequest(const Client& client,
+                                                      const std::chrono::system_clock::time_point& deadline,
+                                                      const std::shared_ptr<internal::Node>& node,
+                                                      proto::TransactionResponse* response) const
+{
+  return node->submitTransaction(
+    proto::TransactionBody::DataCase::kSystemUndelete, makeRequest(client, node), deadline, response);
+}
+
+//-----
+void SystemUndeleteTransaction::addToBody(proto::TransactionBody& body) const
+{
+  body.set_allocated_systemundelete(build());
+}
+
+//-----
+proto::SystemUndeleteTransactionBody* SystemUndeleteTransaction::build() const
+{
+  auto body = std::make_unique<proto::SystemUndeleteTransactionBody>();
+
+  if (mFileId.has_value())
+  {
+    body->set_allocated_fileid(mFileId->toProtobuf().release());
+  }
+  else if (mContractId.has_value())
+  {
+    body->set_allocated_contractid(mContractId->toProtobuf().release());
+  }
+
+  return body.release();
+}
+
+} // namespace Hedera
