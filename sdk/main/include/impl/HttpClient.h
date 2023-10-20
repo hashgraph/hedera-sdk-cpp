41ff3e16da9be412297cfaf4a189161410dc3124
gsstoykov <georgi.stoykov@limechain.tech>
2023-10-20 16:31:04 +0300

    Added curlib dependency to build. Created basic HTTPClient service for querying the MirrorNode
    Signed-off-by: gsstoykov <georgi.stoykov@limechain.tech>

---
+++ sdk/main/include/impl/HttpClient.h
@@ -0,0 +1,57 @@
+#include <curl/curl.h>
+#include <iostream>
+#include <string>
+
+class HttpClient
+{
+public:
+  /**
+   * @brief Constructor for HttpClient.
+   *
+   * This constructor initializes the HttpClient and performs global libcurl initialization
+   * using CURL_GLOBAL_DEFAULT.
+   *
+   * @throw std::runtime_error If libcurl initialization fails.
+   */
+  HttpClient();
+
+  HttpClient(const HttpClient&) = delete;            // no need as only one httpClient could be used
+  HttpClient& operator=(const HttpClient&) = delete; // no need as only one httpClient could be used
+
+  /**
+   * @brief Destructor for HttpClient.
+   *
+   * This destructor cleans up global libcurl resources using curl_global_cleanup.
+   * It should be called when an HttpClient instance is no longer needed to release libcurl resources.
+   */
+  ~HttpClient();
+  /**
+   * @brief Fetches data from the specified URL using the provided RPC method.
+   * @param url The URL to fetch data from.
+   * @param rpcMethod The RPC method.
+   * @return The fetched data as a string.
+   */
+  [[nodiscard]] const std::string invokeRPC(const std::string& url, const std::string& rpcMethod);
+  /**
+   * @brief This invokeREST function creates GET and POST requests.
+   *  Can be further extended for supporting other HTTP
+   *  methods or handle more advanced scenarios as needed.
+   * @param url The URL to fetch data from.
+   * @param httpMethod The HTTP method.
+   * @param requestBody The HTTP request body.
+   * @return The fetched data as a string.
+   */
+  [[nodiscard]] const std::string invokeREST(const std::string& url,
+                                             const std::string& httpMethod,
+                                             const std::string& requestBody);
+  /**
+   * @brief The callback function used for writing fetched data.
+   * @param contents A pointer to the fetched data.
+   * @param size The size of each element.
+   * @param nmemb The number of elements.
+   * @param output A pointer to the output string.
+   * @return The total size of the fetched data.
+   */
+private:
+  static size_t writeCallback(char* contents, size_t size, size_t nmemb, std::string* output);
+};
---
+++ sdk/main/src/impl/HttpClient.cc
@@ -0,0 +1,91 @@
+#include "impl/HttpClient.h"
+
+HttpClient::HttpClient()
+{
+  if (curl_global_init(CURL_GLOBAL_DEFAULT) != 0)
+  {
+    throw std::runtime_error("Failed to initialize libcurl");
+  }
+}
+
+HttpClient::~HttpClient()
+{
+  curl_global_cleanup();
+}
+
+// example mirrorNode query httpClient.invokeREST("https://testnet.mirrornode.hedera.com/api/v1/accounts/" + newAccountId ,"GET","");
+// note: should time out before calling this function because the mirror node is not updated on time if accountID has been created
+// exactly before the call. Works without timeout if the data in the mirror node is there from some seconds beforehand
+const std::string HttpClient::invokeREST(const std::string& url,
+                                         const std::string& httpMethod,
+                                         const std::string& requestBody)
+{
+  std::unique_ptr<CURL, decltype(&curl_easy_cleanup)> curl(curl_easy_init(), curl_easy_cleanup);
+
+  if (!curl)
+  {
+    throw std::runtime_error("CURL initialization failed");
+  }
+
+  curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
+
+  std::string response;
+
+  curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, &HttpClient::writeCallback);
+  curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &response);
+
+  curl_easy_setopt(curl.get(), CURLOPT_CUSTOMREQUEST, httpMethod.c_str());
+
+  if (httpMethod == "POST")
+  {
+    curl_easy_setopt(curl.get(), CURLOPT_POSTFIELDS, requestBody.c_str());
+  }
+
+  CURLcode res = curl_easy_perform(curl.get());
+  if (res != CURLE_OK)
+  {
+    throw std::runtime_error(std::string("Error getting curl result: ") + curl_easy_strerror(res));
+  }
+
+  return response;
+}
+
+// example infura query: rpcMethod = R"({"jsonrpc":"2.0","method":"eth_getTransactionByHash","params":[")" + hash +
+// R"("],"id":1})"
+const std::string HttpClient::invokeRPC(const std::string& url, const std::string& rpcMethod)
+{
+  std::unique_ptr<CURL, decltype(&curl_easy_cleanup)> curl(curl_easy_init(), curl_easy_cleanup);
+  if (!curl)
+  {
+    throw std::runtime_error("Failed to initialize libcurl");
+  }
+
+  curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
+  curl_easy_setopt(curl.get(), CURLOPT_POST, 1L);
+
+  std::shared_ptr<curl_slist> headers(curl_slist_append(nullptr, "Content-Type: application/json"),
+                                      curl_slist_free_all);
+  curl_easy_setopt(curl.get(), CURLOPT_HTTPHEADER, headers.get());
+
+  std::string json_body = rpcMethod;
+  curl_easy_setopt(curl.get(), CURLOPT_POSTFIELDS, json_body.c_str());
+
+  std::string response;
+  curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, writeCallback);
+  curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &response);
+
+  CURLcode res = curl_easy_perform(curl.get());
+  if (res != CURLE_OK)
+  {
+    throw std::runtime_error(std::string("Error getting curl result! ") + curl_easy_strerror(res));
+  }
+
+  return response;
+}
+
+size_t HttpClient::writeCallback(char* contents, size_t size, size_t nmemb, std::string* output)
+{
+  size_t totalSize = size * nmemb;
+  output->append(contents, totalSize);
+  return totalSize;
+};
