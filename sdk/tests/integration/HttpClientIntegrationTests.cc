89ca3a9470e868b5bb2fe78be64a27fb6e6e9eea
gsstoykov <georgi.stoykov@limechain.tech>
2023-10-23 18:17:02 +0300

    Fixed local_node.json. Removed redundant code.
    Signed-off-by: gsstoykov <georgi.stoykov@limechain.tech>

--- sdk/tests/integration/HttpClientIntegrationTests.cc
+++ sdk/tests/integration/HttpClientIntegrationTests.cc
@@ -21,7 +21,6 @@
 #include "BaseIntegrationTest.h"
 #include "impl/HttpClient.h"
 
-#include <chrono>
 #include <filesystem>
 #include <fstream>
 #include <gtest/gtest.h>
@@ -30,18 +29,17 @@
 #include <string_view>
 
 using json = nlohmann::json;
-using namespace std;
 using namespace Hedera;
 
 class HttpClientIntegrationTest : public BaseIntegrationTest
 {
 protected:
-  [[nodiscard]] inline const std::string getPathToLocalNodeJSON() const { return mFilePath.string(); }
+  [[nodiscard]] inline const std::string_view& getURL() const { return mUrl; }
   [[nodiscard]] inline const std::string_view& getJsonMirrorNetworkTag() const { return mJsonMirrorNetworkTag; }
   [[nodiscard]] inline const std::string_view& getAccountIdStr() const { return mAccountIdStr; }
 
 private:
-  const std::filesystem::path mFilePath = (std::filesystem::current_path() / "local_node.json").string();
+  const std::string_view mUrl = "http://127.0.0.1:5551/api/v1/accounts/";
   const std::string_view mJsonMirrorNetworkTag = "mirrorNetwork";
   const std::string_view mAccountIdStr = "0.0.3";
 };
@@ -52,21 +50,13 @@ TEST_F(HttpClientIntegrationTest, GETAccountFromLocalMirrorNode)
   // Given
   const std::string_view mirrorNetworkTag = getJsonMirrorNetworkTag();
   const std::string_view accountIdStr = getAccountIdStr();
-  const std::string pathToLocalNodeJSON = getPathToLocalNodeJSON();
-
-  std::ifstream is(pathToLocalNodeJSON, std::ios::in);
-  json localNodeJSONData = json::parse(is);
-  is.close();
-
-  std::string url = "http://";
-  url += localNodeJSONData[mirrorNetworkTag][0];
-  url += "/api/v1/accounts/";
-  url += accountIdStr;
+  std::string mUrl (getURL());
+  mUrl += accountIdStr;
 
   // When 
   internal::HttpClient httpClient;
   std::string response;
-  ASSERT_NO_THROW(response = httpClient.invokeREST(url,"GET"));
+  ASSERT_NO_THROW(response = httpClient.invokeREST(mUrl,"GET"));
   
   // Then
   json responseData = json::parse(response);
