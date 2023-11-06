3001ca208f72afd69b64783c86b8e6d2c7039c86
Rob Walworth <robert.walworth@swirldslabs.com>
2023-11-06 15:24:30 -0600

    Add missing fee classes
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

---
+++ sdk/main/include/TransactionFeeSchedule.h
@@ -0,0 +1,132 @@
+/*-
+ *
+ * Hedera C++ SDK
+ *
+ * Copyright (C) 2020 - 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#ifndef HEDERA_SDK_CPP_TRANSACTION_FEE_SCHEDULE_H_
+#define HEDERA_SDK_CPP_TRANSACTION_FEE_SCHEDULE_H_
+
+#include "FeeData.h"
+#include "RequestType.h"
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+namespace proto
+{
+class TransactionFeeSchedule;
+}
+
+namespace Hedera
+{
+/**
+ * The fees for a specific transaction or query based on the fee data.
+ */
+class TransactionFeeSchedule
+{
+public:
+  /**
+   * Construct a TransactionFeeSchedule object from a TransactionFeeSchedule protobuf object.
+   *
+   * @param proto The TransactionFeeSchedule protobuf object from which to construct an TransactionFeeSchedule object.
+   * @return The constructed TransactionFeeSchedule object.
+   */
+  [[nodiscard]] static TransactionFeeSchedule fromProtobuf(const proto::TransactionFeeSchedule& proto);
+
+  /**
+   * Construct a TransactionFeeSchedule object from a byte array.
+   *
+   * @param bytes The byte array from which to construct an TransactionFeeSchedule object.
+   * @return The constructed TransactionFeeSchedule object.
+   */
+  [[nodiscard]] static TransactionFeeSchedule fromBytes(const std::vector<std::byte>& bytes);
+
+  /**
+   * Construct a TransactionFeeSchedule protobuf object from this TransactionFeeSchedule object.
+   *
+   * @return A pointer to the created TransactionFeeSchedule protobuf object.
+   */
+  [[nodiscard]] std::unique_ptr<proto::TransactionFeeSchedule> toProtobuf() const;
+
+  /**
+   * Construct a representative byte array from this TransactionFeeSchedule object.
+   *
+   * @return The byte array representing this TransactionFeeSchedule object.
+   */
+  [[nodiscard]] std::vector<std::byte> toBytes() const;
+
+  /**
+   * Construct a string representation of this TransactionFeeSchedule object.
+   *
+   * @return The string representation of this TransactionFeeSchedule object.
+   */
+  [[nodiscard]] std::string toString() const;
+
+  /**
+   * Set the request type.
+   *
+   * @param type The request type to set.
+   * @return A reference to this TransactionFeeSchedule with the newly-set request type.
+   */
+  inline TransactionFeeSchedule& setRequestType(RequestType type)
+  {
+    mRequestType = type;
+    return *this;
+  }
+
+  /**
+   * Add a fee to this TransactionFeeSchedule.
+   *
+   * @param fee The fee to add.
+   * @return A reference to this TransactionFeeSchedule with the newly-added fee.
+   */
+  inline TransactionFeeSchedule& addFee(const FeeData& fee)
+  {
+    mFees.push_back(fee);
+    return *this;
+  }
+
+  /**
+   * Get the request type.
+   *
+   * @return The request type.
+   */
+  [[nodiscard]] inline RequestType getRequestType() const { return mRequestType; }
+
+  /**
+   * Get the fees.
+   *
+   * @return The fees.
+   */
+  [[nodiscard]] inline std::vector<FeeData> getFees() const { return mFees; }
+
+private:
+  /**
+   * The request to which this fee schedule applies.
+   */
+  RequestType mRequestType;
+
+  /**
+   * The resource price coefficients.
+   */
+  std::vector<FeeData> mFees;
+};
+
+} // namespace Hedera
+
+#endif // HEDERA_SDK_CPP_TRANSACTION_FEE_SCHEDULE_H_
