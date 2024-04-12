a52fd2fe3968a5a24756e8c58708f946bb572156
gsstoykov <georgi.stoykov@limechain.tech>
2024-04-12 09:49:59 +0300

    chore: update protobufs to v.0.49.0-alpha0
    Signed-off-by: gsstoykov <georgi.stoykov@limechain.tech>

--- HederaApi.cmake
+++ HederaApi.cmake
@@ -1,5 +1,5 @@
-set(HAPI_LIBRARY_HASH "ce3a82d2374a7f0824759df98d5debb2fa4191f90b0d944bfb324c2c54a3027b" CACHE STRING "Use the configured hash to verify the Hedera API protobuf library release")
-set(HAPI_LIBRARY_URL "https://github.com/hashgraph/hedera-protobufs-cpp/releases/download/v0.48.1/hapi-library-dccbe68a.tar.gz" CACHE STRING "Use the configured URL to download the Hedera API protobuf library package")
+set(HAPI_LIBRARY_HASH "e0fb2bd142f315c3062ced9850823a452dd9d0c80cd6a5691c68833a99bf60d2" CACHE STRING "Use the configured hash to verify the Hedera API protobuf library release")
+set(HAPI_LIBRARY_URL "https://github.com/hashgraph/hedera-protobufs-cpp/releases/download/v0.49.0-alpha0/hapi-library-526c8ffd.tar.gz" CACHE STRING "Use the configured URL to download the Hedera API protobuf library package")
 
 set(HAPI_LOCAL_LIBRARY_PATH "" CACHE STRING "Overrides the configured HAPI_LIBRARY_URL setting and instead uses the local path to retrieve the artifacts")
 
