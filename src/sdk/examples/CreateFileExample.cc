d94fd5ba19445dead269cb3f244d26a209c0167b
gsstoykov <georgi.stoykov@limechain.tech>
2024-06-26 10:11:06 +0300

    feat: Updated File example to demonstrate File Update/Query (#737)
    Signed-off-by: gsstoykov <georgi.stoykov@limechain.tech>

--- src/sdk/examples/CreateFileExample.cc
+++ src/sdk/examples/CreateFileExample.cc
@@ -19,8 +19,13 @@
  */
 #include "Client.h"
 #include "ED25519PrivateKey.h"
+#include "FileContentsQuery.h"
 #include "FileCreateTransaction.h"
+#include "FileDeleteTransaction.h"
 #include "FileId.h"
+#include "FileInfo.h"
+#include "FileInfoQuery.h"
+#include "FileUpdateTransaction.h"
 #include "PublicKey.h"
 #include "TransactionReceipt.h"
 #include "TransactionResponse.h"
@@ -43,16 +48,53 @@ int main(int argc, char** argv)
   client.setOperator(operatorAccountId, operatorPrivateKey);
 
   // The file is required to be a byte array, you can easily use the bytes of a file instead.
-  std::string fileContents = "Hedera hashgraph is great!";
+  std::string fileStringContents = "Hedera hashgraph is great!";
 
   const FileId fileId = FileCreateTransaction()
                           .setKeys({ client.getOperatorPublicKey() })
-                          .setContents(fileContents)
+                          .setContents(fileStringContents)
                           .execute(client)
                           .getReceipt(client)
                           .mFileId.value();
 
   std::cout << "Created new file with ID " << fileId.toString() << std::endl;
+
+  // Query the newly-created file.
+  FileInfo fileInfo = FileInfoQuery().setFileId(fileId).execute(client);
+  std::cout << "File Info: " << fileInfo.toString() << std::endl;
+
+  // Query the newly-created file contents.
+  FileContents fileContents = FileContentsQuery().setFileId(fileId).execute(client);
+  std::cout << "Contents of file in bytes: " << std::endl;
+  for (std::byte byte : fileContents)
+  {
+    std::cout << std::to_integer<int>(byte) << " ";
+  }
+  std::cout << std::endl;
+
+  // Update file contents
+  std::string fileStringContentsUpdated = "Hedera hashgraph is great! Updated contents.";
+  std::cout << "Updating file contents! " << std::endl;
+  TransactionReceipt txReceipt = FileUpdateTransaction()
+                                   .setFileId(fileId)
+                                   .setKeys({ client.getOperatorPublicKey() })
+                                   .setContents(fileStringContentsUpdated)
+                                   .freezeWith(&client)
+                                   .sign(operatorPrivateKey)
+                                   .execute(client)
+                                   .getReceipt(client);
+
+  FileContents updatedFileContents = FileContentsQuery().setFileId(fileId).execute(client);
+  std::cout << "Updated contents of file: " << std::endl;
+  for (std::byte byte : updatedFileContents)
+  {
+    std::cout << std::to_integer<int>(byte) << " ";
+  }
+  std::cout << std::endl;
+
+  // Delete the newly-created file.
+  txReceipt = FileDeleteTransaction().setFileId(fileId).execute(client).getReceipt(client);
+  std::cout << "Deleted file with response code: " << gStatusToString.at(txReceipt.mStatus) << std::endl;
 
   return 0;
 }
