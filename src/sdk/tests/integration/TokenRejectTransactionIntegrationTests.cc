9a93445daca86b8c56bfd3c771da3dd28a2ac499
gsstoykov <georgi.stoykov@limechain.tech>
2024-07-16 16:09:10 +0300

    feat: Added Integration test and fixed Node not recoginzing TokenReject
    Signed-off-by: gsstoykov <georgi.stoykov@limechain.tech>

--- src/sdk/tests/integration/TokenRejectTransactionIntegrationTests.cc
+++ src/sdk/tests/integration/TokenRejectTransactionIntegrationTests.cc
@@ -17,8 +17,16 @@
  * limitations under the License.
  *
  */
+#include "AccountBalance.h"
+#include "AccountBalanceQuery.h"
+#include "AccountCreateTransaction.h"
 #include "BaseIntegrationTest.h"
+#include "ED25519PrivateKey.h"
+#include "TokenCreateTransaction.h"
 #include "TokenRejectTransaction.h"
+#include "TransactionReceipt.h"
+#include "TransactionResponse.h"
+#include "TransferTransaction.h"
 
 #include <gtest/gtest.h>
 
@@ -27,6 +35,34 @@ using namespace Hedera;
 class TokenRejectTransactionIntegrationTests : public BaseIntegrationTest
 {
 protected:
+  TokenId createToken(const std::shared_ptr<PrivateKey>& operatorKey)
+  {
+    return TokenCreateTransaction()
+      .setTokenName("ffff")
+      .setTokenSymbol("F")
+      .setInitialSupply(100000)
+      .setTreasuryAccountId(AccountId(2ULL))
+      .setAdminKey(operatorKey)
+      .setFreezeKey(operatorKey)
+      .setWipeKey(operatorKey)
+      .setSupplyKey(operatorKey)
+      .setFeeScheduleKey(operatorKey)
+      .execute(getTestClient())
+      .getReceipt(getTestClient())
+      .mTokenId.value();
+  }
+
+  AccountId createAccount(const std::shared_ptr<PrivateKey>& accountKey)
+  {
+    return AccountCreateTransaction()
+      .setKey(accountKey)
+      .setInitialBalance(Hbar(5))
+      .setMaxAutomaticTokenAssociations(1000)
+      .execute(getTestClient())
+      .getReceipt(getTestClient())
+      .mAccountId.value();
+  }
+
 private:
 };
 
@@ -34,8 +70,55 @@ private:
 TEST_F(TokenRejectTransactionIntegrationTests, CanExecuteForFT)
 {
   // Given
-  // When
-  // Then
+  std::shared_ptr<PrivateKey> operatorKey;
+  ASSERT_NO_THROW(
+    operatorKey = ED25519PrivateKey::fromString(
+      "302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137"));
+
+  // create two fts
+  TokenId tokenId1;
+  ASSERT_NO_THROW(tokenId1 = createToken(operatorKey));
+  TokenId tokenId2;
+  ASSERT_NO_THROW(tokenId2 = createToken(operatorKey));
+
+  // create receiver account with auto associations
+  AccountId receiver;
+  std::shared_ptr<PrivateKey> receiverKey = ED25519PrivateKey::generatePrivateKey();
+  ASSERT_NO_THROW(receiver = createAccount(receiverKey));
+
+  // transfer fts to the receiver
+  TransactionReceipt txReceipt;
+  ASSERT_NO_THROW(txReceipt = TransferTransaction()
+                                .addTokenTransfer(tokenId1, getTestClient().getOperatorAccountId().value(), -10)
+                                .addTokenTransfer(tokenId1, receiver, 10)
+                                .addTokenTransfer(tokenId2, getTestClient().getOperatorAccountId().value(), -10)
+                                .addTokenTransfer(tokenId2, receiver, 10)
+                                .execute(getTestClient())
+                                .getReceipt(getTestClient()));
+
+  // reject the tokens
+  EXPECT_NO_THROW(txReceipt = TokenRejectTransaction()
+                                .setOwner(receiver)
+                                .setFts({ tokenId1, tokenId2 })
+                                .freezeWith(&getTestClient())
+                                .sign(receiverKey)
+                                .execute(getTestClient())
+                                .getReceipt(getTestClient()));
+
+  // verify the balance of the receiver is 0
+  AccountBalance balance;
+  EXPECT_NO_THROW(balance = AccountBalanceQuery().setAccountId(receiver).execute(getTestClient()));
+
+  EXPECT_EQ(0, balance.mTokens[tokenId1]);
+  EXPECT_EQ(0, balance.mTokens[tokenId2]);
+
+  // verify the tokens are transferred back to the treasury
+  EXPECT_NO_THROW(
+    balance =
+      AccountBalanceQuery().setAccountId(getTestClient().getOperatorAccountId().value()).execute(getTestClient()));
+
+  EXPECT_EQ(100000, balance.mTokens[tokenId1]);
+  EXPECT_EQ(100000, balance.mTokens[tokenId2]);
 }
 
 //-----
