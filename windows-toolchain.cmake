f9e5b9397ac7000d0e6a6bff6ddaed8261e9e588
Mihail Mihov <mihail.mihov@limechain.tech>
2024-10-18 12:51:35 +0300

    test
    Signed-off-by: Mihail Mihov <mihail.mihov@limechain.tech>

--- windows-toolchain.cmake
+++ windows-toolchain.cmake
@@ -1,11 +1,18 @@
-# windows-toolchain.cmake
+# the name of the target operating system
 set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# Compiler and linker for MinGW
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
-set(CMAKE_AR x86_64-w64-mingw32-ar)
-set(CMAKE_RANLIB x86_64-w64-mingw32-ranlib)
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-gcc)
+
+# where is the target environment located
+#set(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc
+ #   /home/alex/mingw-install)
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
