f263b2a473c93171d69837670fc4a06b8b3ff80d
Mihail Mihov <mihail.mihov@limechain.tech>
2024-10-18 13:03:11 +0300

    test
    Signed-off-by: Mihail Mihov <mihail.mihov@limechain.tech>

--- windows-toolchain.cmake
+++ windows-toolchain.cmake
@@ -1,9 +1,9 @@
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Windows)
 
-# which compilers to use for C and C++
-set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-gcc)
+# # which compilers to use for C and C++
+# set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+# set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-gcc)
 
 # where is the target environment located
 #set(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc
